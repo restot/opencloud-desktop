restot/opencloud-desktop

commit 054a62d600b36f706372b4e162aa16ad59266e6b

Add error message for trailing spaces on Windows #4878

csync/src/csync_exclude.c
@@ -229,8 +229,12 @@ static CSYNC_EXCLUDE_TYPE _csync_excluded_common(c_strlist_t *excludes, const ch
     // distinguish files ending in '.' from files without an ending,
     // as '.' is a separator that is not stored internally, so let's
     // not allow to sync those to avoid file loss/ambiguities (#416)
-    if (blen > 1 && (bname[blen-1]== ' ' || bname[blen-1]== '.' )) {
-        match = CSYNC_FILE_EXCLUDE_INVALID_CHAR;
+    if (blen > 1) {
+        if (bname[blen-1]== ' ') {
+            match = CSYNC_FILE_EXCLUDE_TRAILING_SPACE;
+        } else if (bname[blen-1]== '.' ) {
+            match = CSYNC_FILE_EXCLUDE_INVALID_CHAR;
+        }
         goto out;
     }
 

csync/src/csync.h
@@ -98,6 +98,7 @@ enum csync_status_codes_e {
     CSYNC_STATUS_INDIVIDUAL_IS_SYMLINK,
     CSYNC_STATUS_INDIVIDUAL_IGNORE_LIST,
     CSYNC_STATUS_INDIVIDUAL_IS_INVALID_CHARS,
+    CSYNC_STATUS_INDIVIDUAL_TRAILING_SPACE,
     CSYNC_STATUS_INDIVIDUAL_EXCLUDE_LONG_FILENAME,
     CYSNC_STATUS_FILE_LOCKED_OR_OPEN,
     CSYNC_STATUS_INDIVIDUAL_EXCLUDE_HIDDEN,

csync/src/csync_exclude.h
@@ -27,6 +27,7 @@ enum csync_exclude_type_e {
   CSYNC_FILE_EXCLUDE_AND_REMOVE,
   CSYNC_FILE_EXCLUDE_LIST,
   CSYNC_FILE_EXCLUDE_INVALID_CHAR,
+  CSYNC_FILE_EXCLUDE_TRAILING_SPACE,
   CSYNC_FILE_EXCLUDE_LONG_FILENAME,
   CSYNC_FILE_EXCLUDE_HIDDEN,
   CSYNC_FILE_EXCLUDE_STAT_FAILED

csync/src/csync_update.c
@@ -477,6 +477,8 @@ out:
               st->error_status = CSYNC_STATUS_INDIVIDUAL_IGNORE_LIST; /* File listed on ignore list. */
           } else if (excluded == CSYNC_FILE_EXCLUDE_INVALID_CHAR) {
               st->error_status = CSYNC_STATUS_INDIVIDUAL_IS_INVALID_CHARS;  /* File contains invalid characters. */
+          } else if (excluded == CSYNC_FILE_EXCLUDE_TRAILING_SPACE) {
+              st->error_status = CSYNC_STATUS_INDIVIDUAL_TRAILING_SPACE; /* File ends with a trailing space. */
           } else if (excluded == CSYNC_FILE_EXCLUDE_LONG_FILENAME) {
               st->error_status = CSYNC_STATUS_INDIVIDUAL_EXCLUDE_LONG_FILENAME; /* File name is too long. */
           } else if (excluded == CSYNC_FILE_EXCLUDE_HIDDEN ) {

src/libsync/syncengine.cpp
@@ -437,6 +437,9 @@ int SyncEngine::treewalkFile( TREE_WALK_FILE *file, bool remote )
     case CSYNC_STATUS_INDIVIDUAL_IS_INVALID_CHARS:
         item->_errorString = tr("Filename contains invalid characters that can not be synced cross platform.");
         break;
+    case CSYNC_STATUS_INDIVIDUAL_TRAILING_SPACE:
+        item->_errorString = tr("Filename contains trailing spaces.");
+        break;
     case CSYNC_STATUS_INDIVIDUAL_EXCLUDE_LONG_FILENAME:
         item->_errorString = tr("Filename is too long.");
         break;

commit 86371d27709e8565581f37de69af8a2d1b5664c1

Excludes: Fix a Windows specific bug #5039
It meant that almost nothing was being excluded.

Broken since 054a62d600b36f706372b4e162aa16ad59266e6b

csync/src/csync_exclude.c
@@ -246,10 +246,11 @@ static CSYNC_EXCLUDE_TYPE _csync_excluded_common(c_strlist_t *excludes, const ch
     if (blen > 1) {
         if (bname[blen-1]== ' ') {
             match = CSYNC_FILE_EXCLUDE_TRAILING_SPACE;
+            goto out;
         } else if (bname[blen-1]== '.' ) {
             match = CSYNC_FILE_EXCLUDE_INVALID_CHAR;
+            goto out;
         }
-        goto out;
     }
 
     if (csync_is_windows_reserved_word(bname)) {

csync/tests/csync_tests/check_csync_exclude.c
@@ -178,6 +178,20 @@ static void check_csync_excluded(void **state)
 
     rc = csync_excluded_no_ctx(csync->excludes, "latex/songbook/my_manuscript.tex.tmp", CSYNC_FTW_TYPE_FILE);
     assert_int_equal(rc, CSYNC_FILE_EXCLUDE_LIST);
+
+#ifdef _WIN32
+    rc = csync_excluded_no_ctx(csync->excludes, "file_trailing_space ", CSYNC_FTW_TYPE_FILE);
+    assert_int_equal(rc, CSYNC_FILE_EXCLUDE_TRAILING_SPACE);
+
+    rc = csync_excluded_no_ctx(csync->excludes, "file_trailing_dot.", CSYNC_FTW_TYPE_FILE);
+    assert_int_equal(rc, CSYNC_FILE_EXCLUDE_INVALID_CHAR);
+
+    rc = csync_excluded_no_ctx(csync->excludes, "AUX", CSYNC_FTW_TYPE_FILE);
+    assert_int_equal(rc, CSYNC_FILE_EXCLUDE_INVALID_CHAR);
+
+    rc = csync_excluded_no_ctx(csync->excludes, "file_invalid_char<", CSYNC_FTW_TYPE_FILE);
+    assert_int_equal(rc, CSYNC_FILE_EXCLUDE_INVALID_CHAR);
+#endif
 }
 
 static void check_csync_excluded_traversal(void **state)
@@ -304,13 +318,14 @@ static void check_csync_excluded_performance(void **state)
 
     const int N = 10000;
     int totalRc = 0;
+    int i = 0;
 
     // Being able to use QElapsedTimer for measurement would be nice...
     {
         struct timeval before, after;
         gettimeofday(&before, 0);
 
-        for (int i = 0; i < N; ++i) {
+        for (i = 0; i < N; ++i) {
             totalRc += csync_excluded_no_ctx(csync->excludes, "/this/is/quite/a/long/path/with/many/components", CSYNC_FTW_TYPE_DIR);
             totalRc += csync_excluded_no_ctx(csync->excludes, "/1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20/21/22/23/24/25/26/27/29", CSYNC_FTW_TYPE_FILE);
         }
@@ -328,7 +343,7 @@ static void check_csync_excluded_performance(void **state)
         struct timeval before, after;
         gettimeofday(&before, 0);
 
-        for (int i = 0; i < N; ++i) {
+        for (i = 0; i < N; ++i) {
             totalRc += csync_excluded_traversal(csync->excludes, "/this/is/quite/a/long/path/with/many/components", CSYNC_FTW_TYPE_DIR);
             totalRc += csync_excluded_traversal(csync->excludes, "/1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20/21/22/23/24/25/26/27/29", CSYNC_FTW_TYPE_FILE);
         }