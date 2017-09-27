restot/opencloud-desktop

commit c39ac68ea3077c33a118a066b81362ec2798c3c0

Replace the custom rbtree by a std::map

src/csync/csync_private.h
@@ -36,6 +36,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <sqlite3.h>
+#include <map>
 
 #include "config_csync.h"
 #include "std/c_lib.h"
@@ -69,6 +70,14 @@ enum csync_replica_e {
  * @brief csync public structure
  */
 struct OCSYNC_EXPORT csync_s {
+  class FileMap : public std::map<QByteArray, std::unique_ptr<csync_file_stat_t>> {
+  public:
+      csync_file_stat_t *findFile(const QByteArray &key) const {
+          auto it = find(key);
+          return it != end() ? it->second.get() : nullptr;
+      }
+  };
+
   struct {
       csync_auth_callback auth_function = nullptr;
       void *userdata = nullptr;
@@ -76,8 +85,8 @@ struct OCSYNC_EXPORT csync_s {
       void *update_callback_userdata = nullptr;
 
       /* hooks for checking the white list (uses the update_callback_userdata) */
-      int (*checkSelectiveSyncBlackListHook)(void*, const char*) = nullptr;
-      int (*checkSelectiveSyncNewFolderHook)(void*, const char* /* path */, const char* /* remotePerm */) = nullptr;
+      int (*checkSelectiveSyncBlackListHook)(void*, const QByteArray &) = nullptr;
+      int (*checkSelectiveSyncNewFolderHook)(void*, const QByteArray &/* path */, const QByteArray &/* remotePerm */) = nullptr;
 
 
       csync_vio_opendir_hook remote_opendir_hook = nullptr;
@@ -105,17 +114,17 @@ struct OCSYNC_EXPORT csync_s {
   } statedb;
 
   struct {
-    std::map<std::string, std::string> folder_renamed_to; // map from->to
-    std::map<std::string, std::string> folder_renamed_from; // map to->from
+    std::map<QByteArray, QByteArray> folder_renamed_to; // map from->to
+    std::map<QByteArray, QByteArray> folder_renamed_from; // map to->from
   } renames;
 
   struct {
     char *uri = nullptr;
-    c_rbtree_t *tree = nullptr;
+    FileMap files;
   } local;
 
   struct {
-    c_rbtree_t *tree = nullptr;
+    FileMap files;
     bool read_from_db = false;
     QByteArray root_perms; /* Permission of the root folder. (Since the root folder is not in the db tree, we need to keep a separate entry.) */
   } remote;
@@ -151,6 +160,13 @@ struct OCSYNC_EXPORT csync_s {
   csync_s(const char *localUri, const char *db_file);
   ~csync_s();
   int reinitialize();
+
+  // For some reason MSVC references the copy constructor and/or the assignment operator
+  // if a class is exported. This is a problem since unique_ptr isn't copyable.
+  // Explicitly disable them to fix the issue.
+  // https://social.msdn.microsoft.com/Forums/en-US/vcgeneral/thread/e39ab33d-1aaf-4125-b6de-50410d9ced1d
+  csync_s(const csync_s &) = delete;
+  csync_s &operator=(const csync_s &) = delete;
 };
 
 /*

src/csync/csync_reconcile.cpp
@@ -37,33 +37,28 @@
 
 /* Check if a file is ignored because one parent is ignored.
  * return the node of the ignored directoy if it's the case, or NULL if it is not ignored */
-static c_rbnode_t *_csync_check_ignored(c_rbtree_t *tree, const char *path, int pathlen) {
-    uint64_t h = 0;
-    c_rbnode_t *node = NULL;
-
+static csync_file_stat_t *_csync_check_ignored(csync_s::FileMap *tree, const QByteArray &path) {
     /* compute the size of the parent directory */
-    int parentlen = pathlen - 1;
-    while (parentlen > 0 && path[parentlen] != '/') {
+    int parentlen = path.size() - 1;
+    while (parentlen > 0 && path.at(parentlen) != '/') {
         parentlen--;
     }
     if (parentlen <= 0) {
-        return NULL;
+        return nullptr;
     }
-
-    h = c_jhash64((uint8_t *) path, parentlen, 0);
-    node = c_rbtree_find(tree, &h);
-    if (node) {
-        csync_file_stat_t *n = (csync_file_stat_t*)node->data;
-        if (n->instruction == CSYNC_INSTRUCTION_IGNORE) {
+    QByteArray parentPath = path.left(parentlen);
+    csync_file_stat_t *fs = tree->findFile(parentPath);
+    if (fs) {
+        if (fs->instruction == CSYNC_INSTRUCTION_IGNORE) {
             /* Yes, we are ignored */
-            return node;
+            return fs;
         } else {
             /* Not ignored */
-            return NULL;
+            return nullptr;
         }
     } else {
         /* Try if the parent itself is ignored */
-        return _csync_check_ignored(tree, path, parentlen);
+        return _csync_check_ignored(tree, parentPath);
     }
 }
 
@@ -83,7 +78,7 @@ static bool _csync_is_collision_safe_hash(const char *checksum_header)
 /**
  * The main function in the reconcile pass.
  *
- * It's called for each entry in the local and remote rbtrees by
+ * It's called for each entry in the local and remote files by
  * csync_reconcile()
  *
  * Before the reconcile phase the trees already know about changes
@@ -107,52 +102,37 @@ static bool _csync_is_collision_safe_hash(const char *checksum_header)
  * (timestamp is newer), it is not overwritten. If both files, on the
  * source and the destination, have been changed, the newer file wins.
  */
-static int _csync_merge_algorithm_visitor(void *obj, void *data) {
-    csync_file_stat_t *cur = NULL;
-    csync_file_stat_t *other = NULL;
+static int _csync_merge_algorithm_visitor(csync_file_stat_t *cur, CSYNC * ctx) {
     std::unique_ptr<csync_file_stat_t> tmp;
-    uint64_t h = 0;
-    int len = 0;
 
-    CSYNC *ctx = NULL;
-    c_rbtree_t *tree = NULL;
-    c_rbnode_t *node = NULL;
-
-    cur = (csync_file_stat_t *) obj;
-    ctx = (CSYNC *) data;
+    csync_s::FileMap *other_tree = nullptr;
 
     /* we need the opposite tree! */
     switch (ctx->current) {
     case LOCAL_REPLICA:
-        tree = ctx->remote.tree;
+        other_tree = &ctx->remote.files;
         break;
     case REMOTE_REPLICA:
-        tree = ctx->local.tree;
+        other_tree = &ctx->local.files;
         break;
     default:
         break;
     }
 
-    node = c_rbtree_find(tree, &cur->phash);
+    csync_file_stat_t *other = other_tree->findFile(cur->path);;
 
-    if (!node) {
+    if (!other) {
         /* Check the renamed path as well. */
-        char *renamed_path = csync_rename_adjust_path(ctx, cur->path);
-        if (renamed_path != cur->path) {
-            len = strlen( renamed_path );
-            h = c_jhash64((uint8_t *) renamed_path, len, 0);
-            node = c_rbtree_find(tree, &h);
-        }
-        SAFE_FREE(renamed_path);
+        other = other_tree->findFile(csync_rename_adjust_path(ctx, cur->path));
     }
-    if (!node) {
+    if (!other) {
         /* Check if it is ignored */
-        node = _csync_check_ignored(tree, cur->path, cur->path.size());
+        other = _csync_check_ignored(other_tree, cur->path);
         /* If it is ignored, other->instruction will be  IGNORE so this one will also be ignored */
     }
 
     /* file only found on current replica */
-    if (node == NULL) {
+    if (!other) {
         switch(cur->instruction) {
         /* file has been modified */
         case CSYNC_INSTRUCTION_EVAL:
@@ -187,26 +167,20 @@ static int _csync_merge_algorithm_visitor(void *obj, void *data) {
             }
 
             if( tmp ) {
-                len = strlen( tmp->path );
-                if( len > 0 ) {
-                    h = c_jhash64((uint8_t *) tmp->path.constData(), len, 0);
+                if( !tmp->path.isEmpty() ) {
                     /* First, check that the file is NOT in our tree (another file with the same name was added) */
-                    node = c_rbtree_find(ctx->current == REMOTE_REPLICA ? ctx->remote.tree : ctx->local.tree, &h);
-                    if (node) {
+                    csync_s::FileMap *our_tree = ctx->current == REMOTE_REPLICA ? &ctx->remote.files : &ctx->local.files;
+	                if (our_tree->findFile(tmp->path)) {
                         CSYNC_LOG(CSYNC_LOG_PRIORITY_TRACE, "Origin found in our tree : %s", tmp->path.constData());
                     } else {
-                        /* Find the temporar file in the other tree. */
-                        node = c_rbtree_find(tree, &h);
-                        CSYNC_LOG(CSYNC_LOG_PRIORITY_TRACE, "PHash of temporary opposite (%s): %" PRIu64 " %s",
-                                tmp->path.constData() , h, node ? "found": "not found" );
-                        if (node) {
-                            other = (csync_file_stat_t*)node->data;
-                        } else {
-                            /* the renamed file could not be found in the opposite tree. That is because it
-                            * is not longer existing there, maybe because it was renamed or deleted.
-                            * The journal is cleaned up later after propagation.
-                            */
-                        }
+                    /* Find the temporar file in the other tree.
+                    * If the renamed file could not be found in the opposite tree, that is because it
+                    * is not longer existing there, maybe because it was renamed or deleted.
+                    * The journal is cleaned up later after propagation.
+                    */
+                    other = other_tree->findFile(tmp->path);
+                    CSYNC_LOG(CSYNC_LOG_PRIORITY_TRACE, "Temporary opposite (%s) %s",
+                            tmp->path.constData() , other ? "found": "not found" );
                     }
                 }
 
@@ -250,7 +224,6 @@ static int _csync_merge_algorithm_visitor(void *obj, void *data) {
         /*
      * file found on the other replica
      */
-        other = (csync_file_stat_t *) node->data;
 
         switch (cur->instruction) {
         case CSYNC_INSTRUCTION_UPDATE_METADATA:
@@ -395,25 +368,26 @@ static int _csync_merge_algorithm_visitor(void *obj, void *data) {
 }
 
 int csync_reconcile_updates(CSYNC *ctx) {
-  int rc;
-  c_rbtree_t *tree = NULL;
+  csync_s::FileMap *tree = nullptr;
 
   switch (ctx->current) {
     case LOCAL_REPLICA:
-      tree = ctx->local.tree;
+      tree = &ctx->local.files;
       break;
     case REMOTE_REPLICA:
-      tree = ctx->remote.tree;
+      tree = &ctx->remote.files;
       break;
     default:
       break;
   }
 
-  rc = c_rbtree_walk(tree, (void *) ctx, _csync_merge_algorithm_visitor);
-  if( rc < 0 ) {
-    ctx->status_code = CSYNC_STATUS_RECONCILE_ERROR;
+  for (auto &pair : *tree) {
+    if (_csync_merge_algorithm_visitor(pair.second.get(), ctx) < 0) {
+      ctx->status_code = CSYNC_STATUS_RECONCILE_ERROR;
+      return -1;
+    }
   }
-  return rc;
+  return 0;
 }
 
 /* vim: set ts=8 sw=2 et cindent: */

src/csync/csync.cpp
@@ -51,36 +51,6 @@
 #include "csync_rename.h"
 #include "c_jhash.h"
 
-static int _key_cmp(const void *key, const void *data) {
-  uint64_t a;
-  csync_file_stat_t *b;
-
-  a = *(uint64_t *) (key);
-  b = (csync_file_stat_t *) data;
-
-  if (a < b->phash) {
-    return -1;
-  } else if (a > b->phash) {
-    return 1;
-  }
-
-  return 0;
-}
-
-static int _data_cmp(const void *key, const void *data) {
-  csync_file_stat_t *a, *b;
-
-  a = (csync_file_stat_t *) key;
-  b = (csync_file_stat_t *) data;
-
-  if (a->phash < b->phash) {
-    return -1;
-  } else if (a->phash > b->phash) {
-    return 1;
-  }
-
-  return 0;
-}
 
 csync_s::csync_s(const char *localUri, const char *db_file) {
   size_t len = 0;
@@ -90,9 +60,6 @@ csync_s::csync_s(const char *localUri, const char *db_file) {
   while(len > 0 && localUri[len - 1] == '/') --len;
 
   local.uri = c_strndup(localUri, len);
-
-  c_rbtree_create(&local.tree, _key_cmp, _data_cmp);
-  c_rbtree_create(&remote.tree, _key_cmp, _data_cmp);
 
   statedb.file = c_strdup(db_file);
 }
@@ -137,7 +104,7 @@ int csync_update(CSYNC *ctx) {
 
   CSYNC_LOG(CSYNC_LOG_PRIORITY_DEBUG,
             "Update detection for local replica took %.2f seconds walking %zu files.",
-            c_secdiff(finish, start), c_rbtree_size(ctx->local.tree));
+            c_secdiff(finish, start), ctx->local.files.size());
   csync_memstat_check();
 
   /* update detection for remote replica */
@@ -157,7 +124,7 @@ int csync_update(CSYNC *ctx) {
   CSYNC_LOG(CSYNC_LOG_PRIORITY_DEBUG,
             "Update detection for remote replica took %.2f seconds "
             "walking %zu files.",
-            c_secdiff(finish, start), c_rbtree_size(ctx->remote.tree));
+            c_secdiff(finish, start), ctx->remote.files.size());
   csync_memstat_check();
 
   ctx->status |= CSYNC_STATUS_UPDATE;
@@ -195,7 +162,7 @@ int csync_reconcile(CSYNC *ctx) {
 
   CSYNC_LOG(CSYNC_LOG_PRIORITY_DEBUG,
       "Reconciliation for local replica took %.2f seconds visiting %zu files.",
-      c_secdiff(finish, start), c_rbtree_size(ctx->local.tree));
+      c_secdiff(finish, start), ctx->local.files.size());
 
   if (rc < 0) {
       if (!CSYNC_STATUS_IS_OK(ctx->status_code)) {
@@ -215,7 +182,7 @@ int csync_reconcile(CSYNC *ctx) {
 
   CSYNC_LOG(CSYNC_LOG_PRIORITY_DEBUG,
       "Reconciliation for remote replica took %.2f seconds visiting %zu files.",
-      c_secdiff(finish, start), c_rbtree_size(ctx->remote.tree));
+      c_secdiff(finish, start), ctx->remote.files.size());
 
   if (rc < 0) {
       if (!CSYNC_STATUS_IS_OK(ctx->status_code)) {
@@ -236,17 +203,11 @@ out:
 /*
  * local visitor which calls the user visitor with repacked stat info.
  */
-static int _csync_treewalk_visitor(void *obj, void *data) {
+static int _csync_treewalk_visitor(csync_file_stat_t *cur, CSYNC * ctx) {
     int rc = 0;
-    csync_file_stat_t *cur         = NULL;
-    CSYNC *ctx                     = NULL;
     csync_treewalk_visit_func *visitor   = NULL;
     _csync_treewalk_context *twctx = NULL;
-    c_rbtree_t *other_tree = NULL;
-    c_rbnode_t *other_node = NULL;
-
-    cur = (csync_file_stat_t *) obj;
-    ctx = (CSYNC *) data;
+    csync_s::FileMap *other_tree = nullptr;
 
     if (ctx == NULL) {
       return -1;
@@ -255,51 +216,33 @@ static int _csync_treewalk_visitor(void *obj, void *data) {
     /* we need the opposite tree! */
     switch (ctx->current) {
     case LOCAL_REPLICA:
-        other_tree = ctx->remote.tree;
+        other_tree = &ctx->remote.files;
         break;
     case REMOTE_REPLICA:
-        other_tree = ctx->local.tree;
+        other_tree = &ctx->local.files;
         break;
     default:
         break;
     }
 
-    other_node = c_rbtree_find(other_tree, &cur->phash);
+    csync_s::FileMap::const_iterator other_file_it = other_tree->find(cur->path);
 
-    if (!other_node) {
+    if (other_file_it == other_tree->cend()) {
         /* Check the renamed path as well. */
-        int len;
-        uint64_t h = 0;
-        char *renamed_path = csync_rename_adjust_path(ctx, cur->path);
-
-        if (!c_streq(renamed_path, cur->path)) {
-            len = strlen( renamed_path );
-            h = c_jhash64((uint8_t *) renamed_path, len, 0);
-            other_node = c_rbtree_find(other_tree, &h);
-        }
-        SAFE_FREE(renamed_path);
+        QByteArray renamed_path = csync_rename_adjust_path(ctx, cur->path);
+        if (renamed_path != cur->path)
+            other_file_it = other_tree->find(renamed_path);
     }
 
-    if (!other_node) {
+    if (other_file_it == other_tree->cend()) {
         /* Check the source path as well. */
-        int len;
-        uint64_t h = 0;
-        char *renamed_path = csync_rename_adjust_path_source(ctx, cur->path);
-
-        if (!c_streq(renamed_path, cur->path)) {
-            len = strlen( renamed_path );
-            h = c_jhash64((uint8_t *) renamed_path, len, 0);
-            other_node = c_rbtree_find(other_tree, &h);
-        }
-        SAFE_FREE(renamed_path);
+        QByteArray renamed_path = csync_rename_adjust_path_source(ctx, cur->path);
+        if (renamed_path != cur->path)
+            other_file_it = other_tree->find(renamed_path);
     }
 
-    csync_file_stat_t *other = other_node ? (csync_file_stat_t*)other_node->data : NULL;
+    csync_file_stat_t *other = (other_file_it != other_tree->cend()) ? other_file_it->second.get() : NULL;
 
-    if (obj == NULL || data == NULL) {
-      ctx->status_code = CSYNC_STATUS_PARAM_ERROR;
-      return -1;
-    }
     ctx->status_code = CSYNC_STATUS_OK;
 
     twctx = (_csync_treewalk_context*) ctx->callbacks.userdata;
@@ -331,28 +274,24 @@ static int _csync_treewalk_visitor(void *obj, void *data) {
  * which calls the local _csync_treewalk_visitor in this module.
  * The user visitor is called from there.
  */
-static int _csync_walk_tree(CSYNC *ctx, c_rbtree_t *tree, csync_treewalk_visit_func *visitor, int filter)
+static int _csync_walk_tree(CSYNC *ctx, csync_s::FileMap *tree, csync_treewalk_visit_func *visitor, int filter)
 {
     _csync_treewalk_context tw_ctx;
-    int rc = -1;
+    int rc = 0;
 
-    if (ctx == NULL) {
-        errno = EBADF;
-        return rc;
-    }
-
-    if (visitor == NULL || tree == NULL) {
-        ctx->status_code = CSYNC_STATUS_PARAM_ERROR;
-        return rc;
-    }
-    
     tw_ctx.userdata = ctx->callbacks.userdata;
     tw_ctx.user_visitor = visitor;
     tw_ctx.instruction_filter = filter;
 
     ctx->callbacks.userdata = &tw_ctx;
 
-    rc = c_rbtree_walk(tree, (void*) ctx, _csync_treewalk_visitor);
+    for (auto &pair : *tree) {
+        if (_csync_treewalk_visitor(pair.second.get(), ctx) < 0) {
+          rc = -1;
+          break;
+        }
+    }
+
     if( rc < 0 ) {
       if( ctx->status_code == CSYNC_STATUS_OK )
           ctx->status_code = csync_errno_to_status(errno, CSYNC_STATUS_TREE_ERROR);
@@ -367,18 +306,9 @@ static int _csync_walk_tree(CSYNC *ctx, c_rbtree_t *tree, csync_treewalk_visit_f
  */
 int csync_walk_remote_tree(CSYNC *ctx,  csync_treewalk_visit_func *visitor, int filter)
 {
-    c_rbtree_t *tree = NULL;
-    int rc = -1;
-
-    if(ctx != NULL) {
-        ctx->status_code = CSYNC_STATUS_OK;
-        ctx->current = REMOTE_REPLICA;
-        tree = ctx->remote.tree;
-    }
-
-    /* all error handling in the called function */
-    rc = _csync_walk_tree(ctx, tree, visitor, filter);
-    return rc;
+    ctx->status_code = CSYNC_STATUS_OK;
+    ctx->current = REMOTE_REPLICA;
+    return _csync_walk_tree(ctx, &ctx->remote.files, visitor, filter);
 }
 
 /*
@@ -386,25 +316,9 @@ int csync_walk_remote_tree(CSYNC *ctx,  csync_treewalk_visit_func *visitor, int 
  */
 int csync_walk_local_tree(CSYNC *ctx, csync_treewalk_visit_func *visitor, int filter)
 {
-    c_rbtree_t *tree = NULL;
-    int rc = -1;
-
-    if (ctx != NULL) {
-        ctx->status_code = CSYNC_STATUS_OK;
-        ctx->current = LOCAL_REPLICA;
-        tree = ctx->local.tree;
-    }
-
-    /* all error handling in the called function */
-    rc = _csync_walk_tree(ctx, tree, visitor, filter);
-    return rc;  
-}
-
-static void _tree_destructor(void *data) {
-  csync_file_stat_t *freedata = NULL;
-
-  freedata = (csync_file_stat_t *) data;
-  delete freedata;
+    ctx->status_code = CSYNC_STATUS_OK;
+    ctx->current = LOCAL_REPLICA;
+    return _csync_walk_tree(ctx, &ctx->local.files, visitor, filter);
 }
 
 int csync_s::reinitialize() {
@@ -419,26 +333,12 @@ int csync_s::reinitialize() {
   }
   statedb.db = NULL;
 
-  /* destroy the rbtrees */
-  if (c_rbtree_size(local.tree) > 0) {
-    c_rbtree_destroy(local.tree, _tree_destructor);
-  }
-
-  if (c_rbtree_size(remote.tree) > 0) {
-    c_rbtree_destroy(remote.tree, _tree_destructor);
-  }
-
-  /* free memory */
-  c_rbtree_free(local.tree);
-  c_rbtree_free(remote.tree);
-
   remote.read_from_db = 0;
   read_remote_from_db = true;
   db_is_empty = false;
 
-  /* Create new trees */
-  c_rbtree_create(&local.tree, _key_cmp, _data_cmp);
-  c_rbtree_create(&remote.tree, _key_cmp, _data_cmp);
+  local.files.clear();
+  remote.files.clear();
 
   status = CSYNC_STATUS_INIT;
   SAFE_FREE(error_string);
@@ -453,19 +353,6 @@ csync_s::~csync_s() {
     CSYNC_LOG(CSYNC_LOG_PRIORITY_WARN, "ERR: closing of statedb failed.");
   }
   statedb.db = NULL;
-
-  /* destroy the rbtrees */
-  if (c_rbtree_size(local.tree) > 0) {
-    c_rbtree_destroy(local.tree, _tree_destructor);
-  }
-
-  if (c_rbtree_size(remote.tree) > 0) {
-    c_rbtree_destroy(remote.tree, _tree_destructor);
-  }
-
-  /* free memory */
-  c_rbtree_free(local.tree);
-  c_rbtree_free(remote.tree);
 
   SAFE_FREE(statedb.file);
   SAFE_FREE(local.uri);

src/csync/csync_rename.cpp
@@ -21,45 +21,43 @@
 #include "csync_private.h"
 #include "csync_rename.h"
 
-#include <string>
-#include <vector>
 #include <algorithm>
 
-static std::string _parentDir(const std::string &path) {
+static QByteArray _parentDir(const QByteArray &path) {
     int len = path.length();
-    while(len > 0 && path[len-1]!='/') len--;
-    while(len > 0 && path[len-1]=='/') len--;
-    return path.substr(0, len);
+    while(len > 0 && path.at(len-1)!='/') len--;
+    while(len > 0 && path.at(len-1)=='/') len--;
+    return path.left(len);
 }
 
-void csync_rename_record(CSYNC* ctx, const char* from, const char* to)
+void csync_rename_record(CSYNC* ctx, const QByteArray &from, const QByteArray &to)
 {
     ctx->renames.folder_renamed_to[from] = to;
     ctx->renames.folder_renamed_from[to] = from;
 }
 
-char* csync_rename_adjust_path(CSYNC* ctx, const char* path)
+QByteArray csync_rename_adjust_path(CSYNC* ctx, const QByteArray &path)
 {
-    for (std::string p = _parentDir(path); !p.empty(); p = _parentDir(p)) {
-        std::map< std::string, std::string >::iterator it = ctx->renames.folder_renamed_to.find(p);
+    for (QByteArray p = _parentDir(path); !p.isEmpty(); p = _parentDir(p)) {
+        std::map< QByteArray, QByteArray >::iterator it = ctx->renames.folder_renamed_to.find(p);
         if (it != ctx->renames.folder_renamed_to.end()) {
-            std::string rep = it->second + (path + p.length());
-            return c_strdup(rep.c_str());
+            QByteArray rep = it->second + path.mid(p.length());
+            return rep;
         }
     }
-    return c_strdup(path);
+    return path;
 }
 
-char* csync_rename_adjust_path_source(CSYNC* ctx, const char* path)
+QByteArray csync_rename_adjust_path_source(CSYNC* ctx, const QByteArray &path)
 {
-    for (std::string p = _parentDir(path); !p.empty(); p = _parentDir(p)) {
-        std::map< std::string, std::string >::iterator it = ctx->renames.folder_renamed_from.find(p);
+    for (QByteArray p = _parentDir(path); !p.isEmpty(); p = _parentDir(p)) {
+        std::map< QByteArray, QByteArray >::iterator it = ctx->renames.folder_renamed_from.find(p);
         if (it != ctx->renames.folder_renamed_from.end()) {
-            std::string rep = it->second + (path + p.length());
-            return c_strdup(rep.c_str());
+            QByteArray rep = it->second + path.mid(p.length());
+            return rep;
         }
     }
-    return c_strdup(path);
+    return path;
 }
 
 bool csync_rename_count(CSYNC *ctx) {

src/csync/csync_rename.h
@@ -23,9 +23,9 @@
 #include "csync.h"
 
 /* Return the final destination path of a given patch in case of renames */
-char OCSYNC_EXPORT *csync_rename_adjust_path(CSYNC *ctx, const char *path);
+QByteArray OCSYNC_EXPORT csync_rename_adjust_path(CSYNC *ctx, const QByteArray &path);
 /* Return the source of a given path in case of renames */
-char OCSYNC_EXPORT *csync_rename_adjust_path_source(CSYNC *ctx, const char *path);
-void OCSYNC_EXPORT csync_rename_record(CSYNC *ctx, const char *from, const char *to);
+QByteArray OCSYNC_EXPORT csync_rename_adjust_path_source(CSYNC *ctx, const QByteArray &path);
+void OCSYNC_EXPORT csync_rename_record(CSYNC *ctx, const QByteArray &from, const QByteArray &to);
 /*  Return the amount of renamed item recorded */
 bool OCSYNC_EXPORT csync_rename_count(CSYNC *ctx);

src/csync/csync_statedb.cpp
@@ -486,18 +486,15 @@ int csync_statedb_get_below_path( CSYNC *ctx, const char *path ) {
             }
 
             /* store into result list. */
-            if (c_rbtree_insert(ctx->remote.tree, (void *) st.release()) < 0) {
-                st.reset();
-                ctx->status_code = CSYNC_STATUS_TREE_ERROR;
-                break;
-            }
+            QByteArray path = st->path;
+            ctx->remote.files[path] = std::move(st);
             cnt++;
         }
     } while( rc == SQLITE_ROW );
 
     ctx->statedb.lastReturnValue = rc;
     if( rc != SQLITE_DONE ) {
-        ctx->status_code = CSYNC_STATUS_TREE_ERROR;
+        ctx->status_code = CSYNC_STATUS_STATEDB_LOAD_ERROR;
     } else {
         CSYNC_LOG(CSYNC_LOG_PRIORITY_DEBUG, "%" PRId64 " entries read below path %s from db.", cnt, path);
     }

src/csync/csync_update.cpp
@@ -383,18 +383,13 @@ out:
   CSYNC_LOG(CSYNC_LOG_PRIORITY_INFO, "file: %s, instruction: %s <<=", fs->path.constData(),
       csync_instruction_str(fs->instruction));
 
+  QByteArray path = fs->path;
   switch (ctx->current) {
     case LOCAL_REPLICA:
-      if (c_rbtree_insert(ctx->local.tree, (void *) fs.release()) < 0) {
-        ctx->status_code = CSYNC_STATUS_TREE_ERROR;
-        return -1;
-      }
+      ctx->local.files[path] = std::move(fs);
       break;
     case REMOTE_REPLICA:
-      if (c_rbtree_insert(ctx->remote.tree, (void *) fs.release()) < 0) {
-        ctx->status_code = CSYNC_STATUS_TREE_ERROR;
-        return -1;
-      }
+      ctx->remote.files[path] = std::move(fs);
       break;
     default:
       break;

src/csync/std/CMakeLists.txt
@@ -17,7 +17,6 @@ set(CSTDLIB_LINK_LIBRARIES
 set(cstdlib_SRCS
   c_alloc.c
   c_path.c
-  c_rbtree.c
   c_string.c
   c_time.c
   c_utf8.cpp

commit 789b01af80ec8d02e3ef73171b7990cd7e8903bf

csync_reconcile: Optimize reconcile by avoiding alocations
Fix performence regression from commit c39ac68ea3077c33a118a066b81362ec2798c3c0
For every new file we would look up every parent directories. Allocating
a new QByteArreay for every parent riectory just to know if it is in the other
tree is wasting lots of CPU.
Use a ByteArrayRef trick, similar to QStringRef

src/csync/csync_private.h
@@ -68,14 +68,44 @@ enum csync_replica_e {
   REMOTE_REPLICA
 };
 
+
+/*
+ * This is a structurere similar to QStringRef
+ * The difference is that it keeps the QByteArray by value and not by pointer
+ * And it only implements a very small subset of the API that is required by csync, the API can be
+ * added as we need it.
+ */
+class ByteArrayRef
+{
+    QByteArray _arr;
+    int _begin = 0;
+    int _size = -1;
+
+public:
+    ByteArrayRef(const QByteArray &arr = {}, int begin = 0, int size = -1)
+        : _arr(arr)
+        , _begin(begin)
+        , _size(qMin(arr.size() - begin, size < 0 ? arr.size() : size))
+    {
+    }
+    ByteArrayRef left(int l) const { return ByteArrayRef(_arr, _begin, l); };
+    char at(int x) const { return _arr.at(_begin + x); }
+    int size() const { return _size; }
+    /* Pointer to the beginning of the data. WARNING: not null terminated */
+    const char *data() const { return _arr.constData() + _begin; }
+
+    friend bool operator==(const ByteArrayRef &a, const ByteArrayRef &b)
+    { return a.size() == b.size() && qstrncmp(a.data(), b.data(), a.size()) == 0; }
+};
+
 /**
  * @brief csync public structure
  */
 struct OCSYNC_EXPORT csync_s {
-  struct FileMapHash { uint operator()(const QByteArray &a) const { return qHash(a); } };
-  class FileMap : public std::unordered_map<QByteArray, std::unique_ptr<csync_file_stat_t>, FileMapHash> {
+  struct FileMapHash { uint operator()(const ByteArrayRef &a) const { return qHashBits(a.data(), a.size()); } };
+  class FileMap : public std::unordered_map<ByteArrayRef, std::unique_ptr<csync_file_stat_t>, FileMapHash> {
   public:
-      csync_file_stat_t *findFile(const QByteArray &key) const {
+      csync_file_stat_t *findFile(const ByteArrayRef &key) const {
           auto it = find(key);
           return it != end() ? it->second.get() : nullptr;
       }

src/csync/csync_reconcile.cpp
@@ -37,7 +37,8 @@ Q_LOGGING_CATEGORY(lcReconcile, "sync.csync.reconciler", QtInfoMsg)
 
 /* Check if a file is ignored because one parent is ignored.
  * return the node of the ignored directoy if it's the case, or NULL if it is not ignored */
-static csync_file_stat_t *_csync_check_ignored(csync_s::FileMap *tree, const QByteArray &path) {
+static csync_file_stat_t *_csync_check_ignored(csync_s::FileMap *tree, const ByteArrayRef &path)
+{
     /* compute the size of the parent directory */
     int parentlen = path.size() - 1;
     while (parentlen > 0 && path.at(parentlen) != '/') {
@@ -46,7 +47,7 @@ static csync_file_stat_t *_csync_check_ignored(csync_s::FileMap *tree, const QBy
     if (parentlen <= 0) {
         return nullptr;
     }
-    QByteArray parentPath = path.left(parentlen);
+    auto parentPath = path.left(parentlen);
     csync_file_stat_t *fs = tree->findFile(parentPath);
     if (fs) {
         if (fs->instruction == CSYNC_INSTRUCTION_IGNORE) {