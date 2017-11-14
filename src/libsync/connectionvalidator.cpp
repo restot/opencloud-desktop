mola32/client

Private links: Account for overwritehost #6146
Retrieve ocs/v1.php/config to determine the desired host and build the
private link urls with that instead.

src/libsync/connectionvalidator.cpp
@@ -18,6 +18,7 @@
 #include <QNetworkReply>
 #include <QNetworkProxyFactory>
 #include <QPixmap>
+#include <QXmlStreamReader>
 
 #include "connectionvalidator.h"
 #include "account.h"
@@ -247,10 +248,22 @@ void ConnectionValidator::slotAuthSuccess()
 
 void ConnectionValidator::checkServerCapabilities()
 {
+    // The main flow now needs the capabilities
     JsonApiJob *job = new JsonApiJob(_account, QLatin1String("ocs/v1.php/cloud/capabilities"), this);
     job->setTimeout(timeoutToUseMsec);
     QObject::connect(job, &JsonApiJob::jsonReceived, this, &ConnectionValidator::slotCapabilitiesRecieved);
     job->start();
+
+    // And we'll retrieve the ocs config in parallel
+    auto ocsConfigUrl = Utility::concatUrlPath(_account->url(), QLatin1String("ocs/v1.php/config"));
+    auto configJob = _account->sendRequest("GET", ocsConfigUrl);
+    configJob->setTimeout(timeoutToUseMsec);
+    // note that 'this' might be destroyed before the job finishes
+    auto account = _account;
+    QObject::connect(configJob, &SimpleNetworkJob::finishedSignal, _account.data(),
+        [=](QNetworkReply *reply) {
+            ocsConfigReceived(reply, account);
+        });
 }
 
 void ConnectionValidator::slotCapabilitiesRecieved(const QJsonDocument &json)
@@ -266,6 +279,41 @@ void ConnectionValidator::slotCapabilitiesRecieved(const QJsonDocument &json)
     }
 
     fetchUser();
+}
+
+void ConnectionValidator::ocsConfigReceived(QNetworkReply *reply, AccountPtr account)
+{
+    if (reply->error() != QNetworkReply::NoError) {
+        qCWarning(lcConnectionValidator) << "Could not retrieve ocs config" << reply->errorString();
+        return;
+    }
+
+    QString host;
+    [&]() { // reading xml has suprisingly complex api, we just want to get /ocs/data/host
+        QXmlStreamReader reader(reply);
+        reader.readNextStartElement();
+        if (reader.name() != QLatin1String("ocs"))
+            return;
+
+        reader.readNextStartElement();
+        while (!reader.atEnd() && !reader.hasError()) {
+            reader.readNextStartElement();
+            if (reader.name() == QLatin1String("data")) {
+                while (!reader.atEnd() && !reader.hasError()) {
+                    reader.readNextStartElement();
+                    if (reader.name() == QLatin1String("host")) {
+                        host = reader.readElementText();
+                    }
+                }
+            }
+        }
+    }();
+    if (host.isEmpty()) {
+        qCWarning(lcConnectionValidator) << "Could not extract 'host' from ocs config reply";
+        return;
+    }
+    qCInfo(lcConnectionValidator) << "Determined user-visible host to be" << host;
+    account->setUserVisibleHost(host);
 }
 
 void ConnectionValidator::fetchUser()

src/libsync/connectionvalidator.h
@@ -58,8 +58,8 @@ namespace OCC {
                               |
   +---------------------------+
   |
-  +-> checkServerCapabilities (cloud/capabilities)
-        JsonApiJob
+  +-> checkServerCapabilities --------------v (in parallel)
+        JsonApiJob (cloud/capabilities)     SimpleNetworkJob (ocs/v1.php/config)
         |
         +-> slotCapabilitiesRecieved -+
                                       |
@@ -129,6 +129,7 @@ private:
     void reportResult(Status status);
     void checkServerCapabilities();
     void fetchUser();
+    static void ocsConfigReceived(QNetworkReply *reply, AccountPtr account);
 
     /** Sets the account's server version
      *

restot/opencloud-desktop

src/libsync/connectionvalidator.cpp
@@ -18,6 +18,7 @@
 #include <QNetworkReply>
 #include <QNetworkProxyFactory>
 #include <QPixmap>
+#include <QXmlStreamReader>
 
 #include "connectionvalidator.h"
 #include "account.h"
@@ -247,10 +248,22 @@ void ConnectionValidator::slotAuthSuccess()
 
 void ConnectionValidator::checkServerCapabilities()
 {
+    // The main flow now needs the capabilities
     JsonApiJob *job = new JsonApiJob(_account, QLatin1String("ocs/v1.php/cloud/capabilities"), this);
     job->setTimeout(timeoutToUseMsec);
     QObject::connect(job, &JsonApiJob::jsonReceived, this, &ConnectionValidator::slotCapabilitiesRecieved);
     job->start();
+
+    // And we'll retrieve the ocs config in parallel
+    // note that 'this' might be destroyed before the job finishes, so intentionally not parented
+    auto configJob = new JsonApiJob(_account, QLatin1String("ocs/v1.php/config"));
+    configJob->setTimeout(timeoutToUseMsec);
+    auto account = _account; // capturing account by value will make it live long enough
+    QObject::connect(configJob, &JsonApiJob::jsonReceived, _account.data(),
+        [=](const QJsonDocument &json) {
+            ocsConfigReceived(json, account);
+        });
+    configJob->start();
 }
 
 void ConnectionValidator::slotCapabilitiesRecieved(const QJsonDocument &json)
@@ -266,6 +279,17 @@ void ConnectionValidator::slotCapabilitiesRecieved(const QJsonDocument &json)
     }
 
     fetchUser();
+}
+
+void ConnectionValidator::ocsConfigReceived(const QJsonDocument &json, AccountPtr account)
+{
+    QString host = json.object().value("ocs").toObject().value("data").toObject().value("host").toString();
+    if (host.isEmpty()) {
+        qCWarning(lcConnectionValidator) << "Could not extract 'host' from ocs config reply";
+        return;
+    }
+    qCInfo(lcConnectionValidator) << "Determined user-visible host to be" << host;
+    account->setUserVisibleHost(host);
 }
 
 void ConnectionValidator::fetchUser()

src/libsync/connectionvalidator.h
@@ -58,9 +58,9 @@ namespace OCC {
                               |
   +---------------------------+
   |
-  +-> checkServerCapabilities (cloud/capabilities)
-        JsonApiJob
-        |
+  +-> checkServerCapabilities --------------v (in parallel)
+        JsonApiJob (cloud/capabilities)     JsonApiJob (ocs/v1.php/config)
+        |                                   +-> ocsConfigReceived
         +-> slotCapabilitiesRecieved -+
                                       |
   +-----------------------------------+
@@ -129,6 +129,7 @@ private:
     void reportResult(Status status);
     void checkServerCapabilities();
     void fetchUser();
+    static void ocsConfigReceived(const QJsonDocument &json, AccountPtr account);
 
     /** Sets the account's server version
      *