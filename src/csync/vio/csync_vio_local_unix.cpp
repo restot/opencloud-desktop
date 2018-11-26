liberty1776dtom/client

Fixes after rebase to master
- use vfs suffix in ProcessDirectoryJob
- fix include vfs.h
- fix local vio passing vfs
- fix checksum computation
- vfs mode use
- mingw lambda compile issue

src/csync/vio/csync_vio_local_unix.cpp
@@ -33,7 +33,7 @@
 #include "csync_util.h"
 
 #include "vio/csync_vio_local.h"
-#include "common/vfsplugin.h"
+#include "common/vfs.h"
 
 #include <QtCore/QLoggingCategory>
 
@@ -89,7 +89,7 @@ int csync_vio_local_closedir(csync_vio_handle_t *dhandle) {
   return rc;
 }
 
-std::unique_ptr<csync_file_stat_t> csync_vio_local_readdir(CSYNC *ctx, csync_vio_handle_t *dhandle) {
+std::unique_ptr<csync_file_stat_t> csync_vio_local_readdir(csync_vio_handle_t *dhandle, OCC::Vfs *vfs) {
 
   dhandle_t *handle = NULL;
 
@@ -141,8 +141,8 @@ std::unique_ptr<csync_file_stat_t> csync_vio_local_readdir(CSYNC *ctx, csync_vio
   }
 
   // Override type for virtual files if desired
-  if (ctx->vfs)
-      ctx->vfs->statTypeVirtualFile(file_stat.get(), nullptr);
+  if (vfs)
+      vfs->statTypeVirtualFile(file_stat.get(), nullptr);
 
   return file_stat;
 }

src/csync/vio/csync_vio_local_win.cpp
@@ -139,7 +139,7 @@ static time_t FileTimeToUnixTime(FILETIME *filetime, DWORD *remainder)
     }
 }
 
-std::unique_ptr<csync_file_stat_t> csync_vio_local_readdir(CSYNC *ctx, csync_vio_handle_t *dhandle) {
+std::unique_ptr<csync_file_stat_t> csync_vio_local_readdir(csync_vio_handle_t *dhandle, OCC::Vfs *vfs) {
 
   dhandle_t *handle = NULL;
   std::unique_ptr<csync_file_stat_t> file_stat;
@@ -166,12 +166,12 @@ std::unique_ptr<csync_file_stat_t> csync_vio_local_readdir(CSYNC *ctx, csync_vio
   }
   auto path = c_utf8_from_locale(handle->ffd.cFileName);
   if (path == "." || path == "..")
-      return csync_vio_local_readdir(ctx, dhandle);
+      return csync_vio_local_readdir(dhandle, vfs);
 
   file_stat.reset(new csync_file_stat_t);
   file_stat->path = path;
 
-    if (ctx->vfs && ctx->vfs->statTypeVirtualFile(file_stat.get(), &handle->ffd)) {
+    if (vfs && vfs->statTypeVirtualFile(file_stat.get(), &handle->ffd)) {
       // all good
     } else if (handle->ffd.dwFileAttributes & FILE_ATTRIBUTE_REPARSE_POINT) {
       // Detect symlinks, and treat junctions as symlinks too.

restot/opencloud-desktop

src/csync/vio/csync_vio_local_unix.cpp
@@ -33,7 +33,7 @@
 #include "csync_util.h"
 
 #include "vio/csync_vio_local.h"
-#include "common/vfsplugin.h"
+#include "common/vfs.h"
 
 #include <QtCore/QLoggingCategory>
 #include <QtCore/QFile>
@@ -72,7 +72,7 @@ int csync_vio_local_closedir(csync_vio_handle_t *dhandle) {
     return rc;
 }
 
-std::unique_ptr<csync_file_stat_t> csync_vio_local_readdir(CSYNC *ctx, csync_vio_handle_t *handle) {
+std::unique_ptr<csync_file_stat_t> csync_vio_local_readdir(csync_vio_handle_t *handle, OCC::Vfs *vfs) {
 
   struct _tdirent *dirent = NULL;
   std::unique_ptr<csync_file_stat_t> file_stat;
@@ -121,8 +121,8 @@ std::unique_ptr<csync_file_stat_t> csync_vio_local_readdir(CSYNC *ctx, csync_vio
   }
 
   // Override type for virtual files if desired
-  if (ctx->vfs)
-      ctx->vfs->statTypeVirtualFile(file_stat.get(), nullptr);
+  if (vfs)
+      vfs->statTypeVirtualFile(file_stat.get(), nullptr);
 
   return file_stat;
 }

src/csync/vio/csync_vio_local_win.cpp
@@ -113,7 +113,7 @@ static time_t FileTimeToUnixTime(FILETIME *filetime, DWORD *remainder)
     }
 }
 
-std::unique_ptr<csync_file_stat_t> csync_vio_local_readdir(CSYNC *ctx, csync_vio_handle_t *handle) {
+std::unique_ptr<csync_file_stat_t> csync_vio_local_readdir(csync_vio_handle_t *handle, OCC::Vfs *vfs) {
 
   std::unique_ptr<csync_file_stat_t> file_stat;
   DWORD rem;
@@ -137,12 +137,12 @@ std::unique_ptr<csync_file_stat_t> csync_vio_local_readdir(CSYNC *ctx, csync_vio
   }
   auto path = c_utf8_from_locale(handle->ffd.cFileName);
   if (path == "." || path == "..")
-      return csync_vio_local_readdir(ctx, handle);
+      return csync_vio_local_readdir(handle, vfs);
 
   file_stat.reset(new csync_file_stat_t);
   file_stat->path = path;
 
-    if (ctx->vfs && ctx->vfs->statTypeVirtualFile(file_stat.get(), &handle->ffd)) {
+    if (vfs && vfs->statTypeVirtualFile(file_stat.get(), &handle->ffd)) {
       // all good
     } else if (handle->ffd.dwFileAttributes & FILE_ATTRIBUTE_REPARSE_POINT) {
       // Detect symlinks, and treat junctions as symlinks too.