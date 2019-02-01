davincinet/client

Add a noPreFlight branding option
For https://github.com/owncloud/enterprise/issues/2951

src/gui/connectionvalidator.cpp
@@ -24,6 +24,7 @@
 #include "networkjobs.h"
 #include "clientproxy.h"
 #include <creds/abstractcredentials.h>
+#include "theme.h"
 
 namespace OCC {
 
@@ -84,6 +85,12 @@ void ConnectionValidator::systemProxyLookupDone(const QNetworkProxy &proxy)
 // The actual check
 void ConnectionValidator::slotCheckServerAndAuth()
 {
+    if (Theme::instance()->noPreFlight()) {
+        // Skip status.php, directly do the authentication test
+        checkAuthentication();
+        return;
+    }
+
     CheckServerJob *checkJob = new CheckServerJob(_account, this);
     checkJob->setTimeout(timeoutToUseMsec);
     checkJob->setIgnoreCredentialFailure(true);

src/gui/creds/httpcredentialsgui.cpp
@@ -43,18 +43,16 @@ void HttpCredentialsGui::askFromUser()
 
 void HttpCredentialsGui::askFromUserAsync()
 {
+    if (Theme::instance()->noPreFlight()) {
+        performOAuthProcess();
+        return;
+    }
+
     // First, we will check what kind of auth we need.
     auto job = new DetermineAuthTypeJob(_account->sharedFromThis(), this);
     QObject::connect(job, &DetermineAuthTypeJob::authType, this, [this](DetermineAuthTypeJob::AuthType type) {
         if (type == DetermineAuthTypeJob::OAuth) {
-            _asyncAuth.reset(new OAuth(_account, this));
-            _asyncAuth->_expectedUser = _account->davUser();
-            connect(_asyncAuth.data(), &OAuth::result,
-                this, &HttpCredentialsGui::asyncAuthResult);
-            connect(_asyncAuth.data(), &OAuth::destroyed,
-                this, &HttpCredentialsGui::authorisationLinkChanged);
-            _asyncAuth->start();
-            emit authorisationLinkChanged();
+            performOAuthProcess();
         } else if (type == DetermineAuthTypeJob::Basic) {
             showDialog();
         } else {
@@ -64,6 +62,18 @@ void HttpCredentialsGui::askFromUserAsync()
         }
     });
     job->start();
+}
+
+void HttpCredentialsGui::performOAuthProcess()
+{
+    _asyncAuth.reset(new OAuth(_account, this));
+    _asyncAuth->_expectedUser = _account->davUser();
+    connect(_asyncAuth.data(), &OAuth::result,
+        this, &HttpCredentialsGui::asyncAuthResult);
+    connect(_asyncAuth.data(), &OAuth::destroyed,
+        this, &HttpCredentialsGui::authorisationLinkChanged);
+    _asyncAuth->start();
+    emit authorisationLinkChanged();
 }
 
 void HttpCredentialsGui::asyncAuthResult(OAuth::Result r, const QString &user,

src/gui/owncloudsetupwizard.cpp
@@ -195,6 +195,11 @@ void OwncloudSetupWizard::slotSystemProxyLookupDone(const QNetworkProxy &proxy)
 
 void OwncloudSetupWizard::slotFindServer()
 {
+    if (Theme::instance()->noPreFlight()) {
+        _ocWizard->setAuthType(DetermineAuthTypeJob::OAuth);
+        return;
+    }
+
     AccountPtr account = _ocWizard->account();
 
     // Set fake credentials before we check what credential it actually is.

src/libsync/theme.cpp
@@ -566,4 +566,9 @@ bool Theme::showVirtualFilesOption() const
     return ConfigFile().showExperimentalOptions();
 }
 
+bool Theme::noPreFlight() const
+{
+    return false;
+}
+
 } // end namespace client

src/libsync/theme.h
@@ -358,6 +358,15 @@ public:
      */
     virtual bool showVirtualFilesOption() const;
 
+    /**
+     * @brief Open directly the browser for oauth2 without doing any unauthenticated request
+     *
+     * This is a very specific option which only make sense if overrideServerUrl is set.
+     * It also impact redirect handling
+     * https://github.com/owncloud/enterprise/issues/2951
+     */
+    virtual bool noPreFlight() const;
+
 protected:
 #ifndef TOKEN_AUTH_ONLY
     QIcon themeIcon(const QString &name, bool sysTray = false, bool sysTrayMenuVisible = false) const;

restot/opencloud-desktop

src/gui/connectionvalidator.cpp
@@ -24,6 +24,7 @@
 #include "networkjobs.h"
 #include "clientproxy.h"
 #include <creds/abstractcredentials.h>
+#include "theme.h"
 
 namespace OCC {
 
@@ -84,6 +85,12 @@ void ConnectionValidator::systemProxyLookupDone(const QNetworkProxy &proxy)
 // The actual check
 void ConnectionValidator::slotCheckServerAndAuth()
 {
+    if (Theme::instance()->noUnauthedRequests()) {
+        // Skip status.php, directly do the authentication test
+        checkAuthentication();
+        return;
+    }
+
     CheckServerJob *checkJob = new CheckServerJob(_account, this);
     checkJob->setTimeout(timeoutToUseMsec);
     checkJob->setIgnoreCredentialFailure(true);

src/gui/creds/httpcredentialsgui.cpp
@@ -43,18 +43,16 @@ void HttpCredentialsGui::askFromUser()
 
 void HttpCredentialsGui::askFromUserAsync()
 {
+    if (Theme::instance()->noUnauthedRequests()) {
+        performOAuthProcess();
+        return;
+    }
+
     // First, we will check what kind of auth we need.
     auto job = new DetermineAuthTypeJob(_account->sharedFromThis(), this);
     QObject::connect(job, &DetermineAuthTypeJob::authType, this, [this](DetermineAuthTypeJob::AuthType type) {
         if (type == DetermineAuthTypeJob::OAuth) {
-            _asyncAuth.reset(new OAuth(_account, this));
-            _asyncAuth->_expectedUser = _account->davUser();
-            connect(_asyncAuth.data(), &OAuth::result,
-                this, &HttpCredentialsGui::asyncAuthResult);
-            connect(_asyncAuth.data(), &OAuth::destroyed,
-                this, &HttpCredentialsGui::authorisationLinkChanged);
-            _asyncAuth->start();
-            emit authorisationLinkChanged();
+            performOAuthProcess();
         } else if (type == DetermineAuthTypeJob::Basic) {
             showDialog();
         } else {
@@ -64,6 +62,18 @@ void HttpCredentialsGui::askFromUserAsync()
         }
     });
     job->start();
+}
+
+void HttpCredentialsGui::performOAuthProcess()
+{
+    _asyncAuth.reset(new OAuth(_account, this));
+    _asyncAuth->_expectedUser = _account->davUser();
+    connect(_asyncAuth.data(), &OAuth::result,
+        this, &HttpCredentialsGui::asyncAuthResult);
+    connect(_asyncAuth.data(), &OAuth::destroyed,
+        this, &HttpCredentialsGui::authorisationLinkChanged);
+    _asyncAuth->start();
+    emit authorisationLinkChanged();
 }
 
 void HttpCredentialsGui::asyncAuthResult(OAuth::Result r, const QString &user,

src/gui/owncloudsetupwizard.cpp
@@ -195,6 +195,11 @@ void OwncloudSetupWizard::slotSystemProxyLookupDone(const QNetworkProxy &proxy)
 
 void OwncloudSetupWizard::slotFindServer()
 {
+    if (Theme::instance()->noUnauthedRequests()) {
+        _ocWizard->setAuthType(DetermineAuthTypeJob::OAuth);
+        return;
+    }
+
     AccountPtr account = _ocWizard->account();
 
     // Set fake credentials before we check what credential it actually is.

src/libsync/theme.cpp
@@ -566,4 +566,9 @@ bool Theme::showVirtualFilesOption() const
     return ConfigFile().showExperimentalOptions();
 }
 
+bool Theme::noUnauthedRequests() const
+{
+    return false;
+}
+
 } // end namespace client

src/libsync/theme.h
@@ -358,6 +358,15 @@ public:
      */
     virtual bool showVirtualFilesOption() const;
 
+    /**
+     * @brief Open directly the browser for oauth2 without doing any unauthenticated request
+     *
+     * This is a very specific option which only make sense if overrideServerUrl is set.
+     * It also impact redirect handling
+     * https://github.com/owncloud/enterprise/issues/2951
+     */
+    virtual bool noUnauthedRequests() const;
+
 protected:
 #ifndef TOKEN_AUTH_ONLY
     QIcon themeIcon(const QString &name, bool sysTray = false, bool sysTrayMenuVisible = false) const;