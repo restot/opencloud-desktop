restot/opencloud-desktop

commit 911edefcc612f61e8de9cfd85b6746ff1915e394

Don't insert items into the folder model if selectiveSync is not supported
This fixes an assertion in FolderStatusModel::SubFolderInfo::resetSubs
rowCount reported 0 but we actually had items in the model

src/gui/folderstatusmodel.cpp
@@ -619,6 +619,9 @@ void FolderStatusModel::slotUpdateDirectories(const QStringList &list)
     if (!parentInfo) {
         return;
     }
+    if (!parentInfo->_folder->supportsSelectiveSync()) {
+        return;
+    }
     ASSERT(parentInfo->_fetchingJob == job);
     ASSERT(parentInfo->_subs.isEmpty());
 

commit 712b9590411dba21e0e15516e62187764818a2ae

Fix assertion introduced by 911edefcc612f61e8de9cfd85b6746ff1915e394
[ fatal default ]:ASSERT: "last < rowCount(parent)" in file C:\_\17a9f6ae\qtbase-everywhere-src-5.12.9\src\corelib\itemmodels\qabstractitemmodel.cpp, line 2787

911edefcc612f61e8de9cfd85b6746ff1915e394 stopped adding sub folders so we can return the actual size.

src/gui/folderstatusmodel.cpp
@@ -362,8 +362,6 @@ int FolderStatusModel::rowCount(const QModelIndex &parent) const
     auto info = infoForIndex(parent);
     if (!info)
         return 0;
-    if (info->_folder && !info->_folder->supportsSelectiveSync())
-        return 0;
     if (info->hasLabel())
         return 1;
     return info->_subs.count();