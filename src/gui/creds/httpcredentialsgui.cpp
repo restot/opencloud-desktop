restot/opencloud-desktop

commit 0619ddc1a47f35b4b398c6dff33bd6553b1de55e

Use user's display name in local sync directory name

src/gui/newwizard/setupwizardcontroller.cpp
@@ -7,6 +7,7 @@
 #include "gui/folderman.h"
 #include "jobs/checkbasicauthjobfactory.h"
 #include "jobs/resolveurljobfactory.h"
+#include "networkjobs/fetchuserinfojobfactory.h"
 #include "pages/accountconfiguredwizardpage.h"
 #include "pages/basiccredentialssetupwizardpage.h"
 #include "pages/oauthcredentialssetupwizardpage.h"
@@ -233,7 +234,7 @@ void SetupWizardController::nextStep(std::optional<PageIndex> currentPage, std::
                         // username might not be set yet, shouldn't matter, though
                         auto oAuth = new OAuth(_accountBuilder.serverUrl(), QString(), _accessManager, {}, this);
 
-                        connect(oAuth, &OAuth::result, this, [this, newPage](OAuth::Result result, const QString &user, const QString &token, const QString &refreshToken) {
+                        connect(oAuth, &OAuth::result, this, [this, newPage](OAuth::Result result, const QString &userName, const QString &token, const QString &displayName, const QString &refreshToken) {
                             // the button may not be clicked any more, since the server has been shut down right before this signal was emitted by the OAuth instance
                             newPage->disableButtons();
 
@@ -244,7 +245,8 @@ void SetupWizardController::nextStep(std::optional<PageIndex> currentPage, std::
 
                             switch (result) {
                             case OAuth::Result::LoggedIn: {
-                                _accountBuilder.setAuthenticationStrategy(new OAuth2AuthenticationStrategy(user, token, refreshToken));
+                                _accountBuilder.setAuthenticationStrategy(new OAuth2AuthenticationStrategy(userName, token, refreshToken));
+                                _accountBuilder.setDisplayName(displayName);
                                 nextStep(1, std::nullopt);
                                 break;
                             }
@@ -355,13 +357,30 @@ void SetupWizardController::nextStep(std::optional<PageIndex> currentPage, std::
                 _wizardWindow->displayPage(_currentPage, 1);
             };
 
-            connect(checkBasicAuthJob, &CoreJob::finished, this, [moveToFinalPage, checkBasicAuthJob, showCredentialsPageAgain]() {
+            connect(checkBasicAuthJob, &CoreJob::finished, this, [moveToFinalPage, checkBasicAuthJob, showCredentialsPageAgain, this, strategy]() {
                 if (checkBasicAuthJob->success()) {
                     if (checkBasicAuthJob->result().toBool()) {
-                        moveToFinalPage();
+                        auto fetchUserInfoJob = FetchUserInfoJobFactory::fromBasicAuthCredentials(_accessManager, strategy->username(), strategy->password(), this).startJob(_accountBuilder.serverUrl());
+
+                        connect(fetchUserInfoJob, &CoreJob::finished, this, [this, strategy, moveToFinalPage, showCredentialsPageAgain, fetchUserInfoJob] {
+                            if (fetchUserInfoJob->success()) {
+                                auto result = fetchUserInfoJob->result().value<FetchUserInfoResult>();
+
+                                Q_ASSERT(result.userName() == strategy->username());
+
+                                _accountBuilder.setDisplayName(result.displayName());
+
+                                moveToFinalPage();
+                            } else {
+                                showCredentialsPageAgain(tr("Failed to fetch user display name"));
+                            }
+                        });
+
                     } else {
                         showCredentialsPageAgain(tr("Login failed: username and/or password incorrect"));
                     }
+
+
                 } else {
                     showCredentialsPageAgain(tr("Login failed: %1").arg(checkBasicAuthJob->errorMessage()));
                 }

src/libsync/creds/oauth.cpp
@@ -18,9 +18,8 @@
 #include "common/version.h"
 #include "credentialmanager.h"
 #include "creds/httpcredentials.h"
-#include "creds/jobs/determineuserjobfactory.h"
-#include "networkjobs.h"
 #include "networkjobs/checkserverjobfactory.h"
+#include "networkjobs/fetchuserinfojobfactory.h"
 #include "theme.h"
 
 #include <QApplication>
@@ -301,12 +300,8 @@ void OAuth::startAuthentication()
                         emit result(Error);
                         return;
                     }
-                    if (!user.isEmpty()) {
-                        finalize(socket, accessToken, refreshToken, user, messageUrl);
-                        return;
-                    }
 
-                    auto *job = DetermineUserJobFactory(_networkAccessManager, accessToken, this).startJob(_serverUrl);
+                    auto *job = FetchUserInfoJobFactory::fromOAuth2Credentials(_networkAccessManager, accessToken, this).startJob(_serverUrl);
 
                     connect(job, &CoreJob::finished, this, [=]() {
                         if (!job->success()) {
@@ -314,8 +309,8 @@ void OAuth::startAuthentication()
                                 tr("<h1>Login Error</h1><p>%1</p>").arg(job->errorMessage()).toUtf8());
                             emit result(Error);
                         } else {
-                            const QString user = job->result().toString();
-                            finalize(socket, accessToken, refreshToken, user, messageUrl);
+                            auto result = job->result().value<FetchUserInfoResult>();
+                            finalize(socket, accessToken, refreshToken, result.userName(), result.displayName(), messageUrl);
                         }
                     });
                 });
@@ -389,17 +384,16 @@ void OAuth::refreshAuthentication(const QString &refreshToken)
     fetchWellKnown();
 }
 
-void OAuth::finalize(const QPointer<QTcpSocket> &socket, const QString &accessToken,
-    const QString &refreshToken, const QString &user, const QUrl &messageUrl)
+void OAuth::finalize(const QPointer<QTcpSocket> &socket, const QString &accessToken, const QString &refreshToken, const QString &userName, const QString &displayName, const QUrl &messageUrl)
 {
-    if (!_davUser.isEmpty() && user != _davUser) {
+    if (!_davUser.isEmpty() && userName != _davUser) {
         // Connected with the wrong user
-        qCWarning(lcOauth) << "We expected the user" << _davUser << "but the server answered with user" << user;
+        qCWarning(lcOauth) << "We expected the user" << _davUser << "but the server answered with user" << userName;
         const QString message = tr("<h1>Wrong user</h1>"
                                    "<p>You logged-in with user <em>%1</em>, but must login with user <em>%2</em>.<br>"
                                    "Please log out of %3 in another tab, then <a href='%4'>click here</a> "
                                    "and log in as user %2</p>")
-                                    .arg(user, _davUser, Theme::instance()->appNameGUI(),
+                                    .arg(userName, _davUser, Theme::instance()->appNameGUI(),
                                         authorisationLink().toString(QUrl::FullyEncoded));
         httpReplyAndClose(socket, QByteArrayLiteral("403 Forbidden"), message.toUtf8());
         // We are still listening on the socket so we will get the new connection
@@ -412,7 +406,7 @@ void OAuth::finalize(const QPointer<QTcpSocket> &socket, const QString &accessTo
     } else {
         httpReplyAndClose(socket, QByteArrayLiteral("200 OK"), loginSuccessfullHtml);
     }
-    emit result(LoggedIn, user, accessToken, refreshToken);
+    emit result(LoggedIn, userName, accessToken, displayName, refreshToken);
 }
 
 QNetworkReply *OAuth::postTokenRequest(const QList<QPair<QString, QString>> &queryItems)

src/libsync/creds/oauth.h
@@ -79,7 +79,7 @@ Q_SIGNALS:
      * The state has changed.
      * when logged in, token has the value of the token.
      */
-    void result(OAuth::Result result, const QString &user = QString(), const QString &token = QString(), const QString &refreshToken = QString());
+    void result(OAuth::Result result, const QString &userName = QString(), const QString &token = QString(), const QString &displayName = QString(), const QString &refreshToken = QString());
 
     /**
      * emitted when the call to the well-known endpoint is finished
@@ -104,8 +104,7 @@ protected:
     virtual void dynamicRegistrationDataReceived(const QVariantMap &dynamicRegistrationData);
 
 private:
-    void finalize(const QPointer<QTcpSocket> &socket, const QString &accessToken,
-        const QString &refreshToken, const QString &userId, const QUrl &messageUrl);
+    void finalize(const QPointer<QTcpSocket> &socket, const QString &accessToken, const QString &refreshToken, const QString &userName, const QString &displayName, const QUrl &messageUrl);
 
     QNetworkReply *postTokenRequest(const QList<QPair<QString, QString>> &queryItems);
 

test/testoauth.cpp
@@ -97,6 +97,7 @@ public:
         StatusPhpState,
         BrowserOpened,
         TokenAsked,
+        UserInfoFetched,
         CustomState } state = StartState;
     Q_ENUM(State);
 
@@ -126,6 +127,8 @@ public:
                 return this->wellKnownReply(op, req);
             } else if (req.url().path().endsWith("status.php")) {
                 return this->statusPhpReply(op, req);
+            } else if (req.url().path().endsWith("ocs/v2.php/cloud/user") && req.url().query() == "format=json") {
+                return this->userInfoReply(op, req);
             }
             OC_ASSERT(device);
             OC_ASSERT(device && device->bytesAvailable() > 0); // OAuth2 always sends around POST data.
@@ -165,7 +168,7 @@ public:
 
     virtual void browserReplyFinished() {
         QCOMPARE(sender(), browserReply.data());
-        QCOMPARE(state, TokenAsked);
+        QCOMPARE(state, UserInfoFetched);
         browserReply->deleteLater();
         QCOMPARE(QNetworkReply::NoError, browserReply->error());
         QCOMPARE(browserReply->rawHeader("Location"), QByteArray("owncloud://success"));
@@ -193,6 +196,19 @@ public:
         OC_ASSERT(req.url().path() == sOAuthTestServer.path() + "/status.php");
         auto payload = std::make_unique<QBuffer>();
         payload->setData(statusPhpPayload());
+        return new FakePostReply(op, req, std::move(payload), fakeAm);
+    }
+
+    virtual QNetworkReply *userInfoReply(QNetworkAccessManager::Operation op, const QNetworkRequest &req)
+    {
+        OC_ASSERT(state == TokenAsked);
+        state = UserInfoFetched;
+        OC_ASSERT(op == QNetworkAccessManager::GetOperation);
+        OC_ASSERT(req.url().toString().startsWith(sOAuthTestServer.toString()));
+        OC_ASSERT(req.url().path() == sOAuthTestServer.path() + "/ocs/v2.php/cloud/user");
+        OC_ASSERT(req.url().query() == "format=json");
+        auto payload = std::make_unique<QBuffer>();
+        payload->setData(userInfoPayload());
         return new FakePostReply(op, req, std::move(payload), fakeAm);
     }
 
@@ -226,9 +242,24 @@ public:
         return jsondata.toJson();
     }
 
-    virtual void oauthResult(OAuth::Result result, const QString &user, const QString &token , const QString &refreshToken) {
+    virtual QByteArray userInfoPayload() const
+    {
+        // the dummy server provides the user admin
+        // we don't provide "meta" at the moment, since it is not used
+        QJsonDocument jsonData(QJsonObject {
+            { "ocs", QJsonObject { { "data", QJsonObject {
+                                                 { "display-name", "Admin" },
+                                                 { "id", "admin" },
+                                                 { "email", "admin@admin.admin" },
+
+                                             } } } } });
+        return jsonData.toJson();
+    }
+
+    virtual void oauthResult(OAuth::Result result, const QString &user, const QString &token, const QString &displayName, const QString &refreshToken)
+    {
         QCOMPARE(result, OAuth::LoggedIn);
-        QCOMPARE(state, TokenAsked);
+        QCOMPARE(state, UserInfoFetched);
         QCOMPARE(user, QString("admin"));
         QCOMPARE(token, QString("123"));
         QCOMPARE(refreshToken, QString("456"));
@@ -350,10 +381,11 @@ private slots:
                 return OAuthTestCase::tokenReply(op, req);
             }
 
-            void oauthResult(OAuth::Result result, const QString &user, const QString &token ,
-                             const QString &refreshToken) override {
+            void oauthResult(OAuth::Result result, const QString &user, const QString &token,
+                const QString &displayName, const QString &refreshToken) override
+            {
                 if (state != CustomState)
-                    return OAuthTestCase::oauthResult(result, user, token, refreshToken);
+                    return OAuthTestCase::oauthResult(result, user, token, displayName, refreshToken);
                 QCOMPARE(result, OAuth::Error);
             }
         } test;
@@ -416,7 +448,7 @@ private slots:
                 return new FakeHangingReply(op, req, fakeAm);
             }
 
-            void oauthResult(OAuth::Result result, const QString &user, const QString &token, const QString &refreshToken) override
+            void oauthResult(OAuth::Result result, const QString &user, const QString &token, const QString &displayName, const QString &refreshToken) override
             {
                 Q_UNUSED(user);
                 Q_UNUSED(token);

src/gui/newwizard/setupwizardaccountbuilder.cpp
@@ -109,6 +109,8 @@ AccountPtr SetupWizardAccountBuilder::build()
     newAccountPtr->setDavUser(_authenticationStrategy->davUser());
     newAccountPtr->setCredentials(_authenticationStrategy->makeCreds());
 
+    newAccountPtr->setDavDisplayName(_displayName);
+
     newAccountPtr->addApprovedCerts({ _customTrustedCaCertificates.begin(), _customTrustedCaCertificates.end() });
 
     return newAccountPtr;
@@ -125,9 +127,12 @@ bool SetupWizardAccountBuilder::hasValidCredentials() const
 
 QString SetupWizardAccountBuilder::displayName() const
 {
-    Q_ASSERT(hasValidCredentials());
-    // TODO: get the correct display name instead of the dav user
-    return _authenticationStrategy->davUser();
+    return _displayName;
+}
+
+void SetupWizardAccountBuilder::setDisplayName(const QString &displayName)
+{
+    _displayName = displayName;
 }
 
 void SetupWizardAccountBuilder::setAuthenticationStrategy(AbstractAuthenticationStrategy *strategy)

src/gui/newwizard/setupwizardaccountbuilder.h
@@ -117,6 +117,7 @@ public:
     bool hasValidCredentials() const;
 
     QString displayName() const;
+    void setDisplayName(const QString &displayName);
 
     /**
      * Store custom CA certificate for the newly built account.
@@ -137,6 +138,8 @@ public:
 
 private:
     QUrl _serverUrl;
+
+    QString _displayName;
 
     DetermineAuthTypeJob::AuthType _authType = DetermineAuthTypeJob::AuthType::Unknown;
 

src/gui/newwizard/setupwizardcontroller.h
@@ -50,7 +50,7 @@ Q_SIGNALS:
     void finished(AccountPtr newAccount, SyncMode syncMode);
 
 private:
-    void nextStep(std::optional<PageIndex> currentPage, std::optional<PageIndex> desiredPage);
+    void nextStep(std::optional<PageIndex> currentPage, std::optional<PageIndex> userName);
 
     SetupWizardWindow *_wizardWindow;
 

src/libsync/CMakeLists.txt
@@ -45,13 +45,13 @@ set(libsync_SRCS
     syncresult.cpp
     syncoptions.cpp
     theme.cpp
-    creds/jobs/determineuserjobfactory.cpp
     creds/credentialmanager.cpp
     creds/dummycredentials.cpp
     creds/abstractcredentials.cpp
     creds/credentialscommon.cpp
     creds/oauth.cpp
 
+    networkjobs/fetchuserinfojobfactory.cpp
     networkjobs/checkserverjobfactory.cpp
     networkjobs/jsonjob.cpp
 

commit f7a50d16a69f7c3eb5c4c145d793ebd1f3879800

Fix regression introduced in 0619ddc1a47f35b4b398c6dff33bd6553b1de55e

src/gui/creds/httpcredentialsgui.cpp
@@ -88,7 +88,7 @@ void HttpCredentialsGui::askFromUserAsync()
 }
 
 void HttpCredentialsGui::asyncAuthResult(OAuth::Result r, const QString &user,
-    const QString &token, const QString &refreshToken)
+    const QString &token, const QString &refreshToken, const QString &)
 {
     _asyncAuth.reset();
     switch (r) {

src/gui/creds/httpcredentialsgui.h
@@ -53,7 +53,7 @@ public:
 
     static QString requestAppPasswordText(const Account *account);
 private slots:
-    void asyncAuthResult(OAuth::Result, const QString &user, const QString &accessToken, const QString &refreshToken);
+    void asyncAuthResult(OAuth::Result, const QString &user, const QString &accessToken, const QString &refreshToken, const QString &displayName);
     void showDialog();
     void askFromUserAsync();
 

src/gui/newwizard/setupwizardcontroller.cpp
@@ -237,7 +237,7 @@ void SetupWizardController::nextStep(std::optional<PageIndex> currentPage, std::
                         // username might not be set yet, shouldn't matter, though
                         auto oAuth = new OAuth(_accountBuilder.serverUrl(), QString(), _accessManager, {}, this);
 
-                        connect(oAuth, &OAuth::result, this, [this, newPage](OAuth::Result result, const QString &userName, const QString &token, const QString &displayName, const QString &refreshToken) {
+                        connect(oAuth, &OAuth::result, this, [this, newPage](OAuth::Result result, const QString &userName, const QString &token, const QString &refreshToken, const QString &displayName) {
                             // the button may not be clicked any more, since the server has been shut down right before this signal was emitted by the OAuth instance
                             newPage->disableButtons();
 

src/libsync/creds/oauth.cpp
@@ -442,7 +442,7 @@ void OAuth::finalize(const QPointer<QTcpSocket> &socket, const QString &accessTo
     } else {
         httpReplyAndClose(socket, QStringLiteral("200 OK"), loginSuccessfullTitle, loginSuccessfullHtml);
     }
-    emit result(LoggedIn, userName, accessToken, displayName, refreshToken);
+    emit result(LoggedIn, userName, accessToken, refreshToken, displayName);
 }
 
 QNetworkReply *OAuth::postTokenRequest(const QList<QPair<QString, QString>> &queryItems)

src/libsync/creds/oauth.h
@@ -79,7 +79,7 @@ Q_SIGNALS:
      * The state has changed.
      * when logged in, token has the value of the token.
      */
-    void result(OAuth::Result result, const QString &userName = QString(), const QString &token = QString(), const QString &displayName = QString(), const QString &refreshToken = QString());
+    void result(OAuth::Result result, const QString &userName = QString(), const QString &token = QString(), const QString &refreshToken = QString(), const QString &displayName = QString());
 
     /**
      * emitted when the call to the well-known endpoint is finished

test/testoauth.cpp
@@ -256,7 +256,7 @@ public:
         return jsonData.toJson();
     }
 
-    virtual void oauthResult(OAuth::Result result, const QString &user, const QString &token, const QString &displayName, const QString &refreshToken)
+    virtual void oauthResult(OAuth::Result result, const QString &user, const QString &token, const QString &refreshToken, const QString &displayName)
     {
         QCOMPARE(result, OAuth::LoggedIn);
         QCOMPARE(state, UserInfoFetched);