restot/opencloud-desktop

commit b479539d9b08c56d0256c58b58372ab0dbefccd1

Split up controller logic into multiple classes
This is a mix of the state and abstract factory patterns. Moving the functionality into multiple classes reduces the complexity of the controller code and makes adding new states easier.

The controller is still in charge of deciding when to display which page etc., and creates the single objects. It is, however, no longer in charge of creating the page widgets, this is handled by the state objects. While this removes the dependency injection functionality used elsewhere within the new wizard, currently, there is no advantage in having the controller be fully in charge of the creation of the widgets.

src/gui/newwizard/states/oauthcredentialssetupwizardstate.cpp
@@ -1,0 +1,87 @@
+/*
+ * Copyright (C) Fabian Müller <fmueller@owncloud.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+#include <QApplication>
+#include <QClipboard>
+
+#include "oauthcredentialssetupwizardstate.h"
+
+namespace OCC::Wizard {
+
+OAuthCredentialsSetupWizardState::OAuthCredentialsSetupWizardState(SetupWizardContext *context)
+    : AbstractSetupWizardState(context)
+{
+    auto oAuthCredentialsPage = new OAuthCredentialsSetupWizardPage(_context->accountBuilder().serverUrl());
+    _page = oAuthCredentialsPage;
+
+    // username might not be set yet, shouldn't matter, though
+    auto oAuth = new OAuth(_context->accountBuilder().serverUrl(), QString(), _context->accessManager(), {}, this);
+
+    connect(oAuth, &OAuth::result, this, [this, oAuthCredentialsPage](OAuth::Result result, const QString &userName, const QString &token, const QString &displayName, const QString &refreshToken) {
+        // the button may not be clicked anymore, since the server has been shut down right before this signal was emitted by the OAuth instance
+        oAuthCredentialsPage->disableButtons();
+
+        _context->window()->slotStartTransition();
+
+        // bring window up top again, as the browser may have been raised in front of it
+        _context->window()->raise();
+
+        switch (result) {
+        case OAuth::Result::LoggedIn: {
+            _context->accountBuilder().setAuthenticationStrategy(new OAuth2AuthenticationStrategy(userName, token, refreshToken));
+            _context->accountBuilder().setDisplayName(displayName);
+            Q_EMIT evaluationSuccessful();
+            break;
+        }
+        case OAuth::Result::Error: {
+            Q_EMIT evaluationFailed(tr("Error while trying to log into OAuth2-enabled server."));
+            break;
+        }
+        case OAuth::Result::NotSupported: {
+            // should never happen
+            Q_EMIT evaluationFailed(tr("Server reports that OAuth2 is not supported."));
+            break;
+        }
+        }
+    });
+
+    connect(oAuthCredentialsPage, &OAuthCredentialsSetupWizardPage::openBrowserButtonPushed, this, [oAuth]() {
+        oAuth->openBrowser();
+    });
+
+    connect(oAuthCredentialsPage, &OAuthCredentialsSetupWizardPage::copyUrlToClipboardButtonPushed, this, [oAuth]() {
+        // TODO: use authorisationLinkAsync
+        auto link = oAuth->authorisationLink().toString();
+        QApplication::clipboard()->setText(link);
+    });
+
+    // moving to next page is only possible once we see a request to our embedded web server
+    _context->window()->disableNextButton();
+
+    oAuth->startAuthentication();
+}
+
+SetupWizardState OAuthCredentialsSetupWizardState::state() const
+{
+    return SetupWizardState::CredentialsState;
+}
+
+void OAuthCredentialsSetupWizardState::evaluatePage()
+{
+    // the next button is disabled anyway, since moving forward is controlled by the OAuth object signal handlers
+    // therefore, this method should never ever be called
+    Q_UNREACHABLE();
+}
+
+} // OCC::Wizard

src/gui/newwizard/CMakeLists.txt
@@ -40,6 +40,15 @@ add_library(newwizard STATIC
     postfixlineedit.cpp
 
     syncmode.cpp
+
+    states/setupwizardstate.cpp
+    states/abstractsetupwizardstate.cpp
+    states/serverurlsetupwizardstate.cpp
+    states/basiccredentialssetupwizardstate.cpp
+    states/oauthcredentialssetupwizardstate.cpp
+    states/accountconfiguredsetupwizardstate.cpp
+
+    setupwizardcontext.cpp
 )
 target_link_libraries(newwizard PUBLIC Qt5::Widgets libsync)
 set_target_properties(newwizard PROPERTIES AUTOUIC ON AUTORCC ON)

src/gui/newwizard/setupwizardaccountbuilder.cpp
@@ -113,6 +113,10 @@ AccountPtr SetupWizardAccountBuilder::build()
 
     newAccountPtr->addApprovedCerts({ _customTrustedCaCertificates.begin(), _customTrustedCaCertificates.end() });
 
+    if (!_defaultSyncTargetDir.isEmpty()) {
+        newAccountPtr->setDefaultSyncRoot(_defaultSyncTargetDir);
+    }
+
     return newAccountPtr;
 }
 
@@ -153,5 +157,10 @@ void SetupWizardAccountBuilder::clearCustomTrustedCaCertificates()
 AbstractAuthenticationStrategy *SetupWizardAccountBuilder::authenticationStrategy() const
 {
     return _authenticationStrategy.get();
+}
+
+void SetupWizardAccountBuilder::setDefaultSyncTargetDir(const QString &syncTargetDir)
+{
+    _defaultSyncTargetDir = syncTargetDir;
 }
 }

src/gui/newwizard/setupwizardaccountbuilder.h
@@ -119,6 +119,9 @@ public:
     QString displayName() const;
     void setDisplayName(const QString &displayName);
 
+    // getter is not needed at the moment
+    void setDefaultSyncTargetDir(const QString &syncTargetDir);
+
     /**
      * Store custom CA certificate for the newly built account.
      * @param customTrustedCaCertificate certificate to store
@@ -146,5 +149,7 @@ private:
     std::unique_ptr<AbstractAuthenticationStrategy> _authenticationStrategy;
 
     QSet<QSslCertificate> _customTrustedCaCertificates;
+
+    QString _defaultSyncTargetDir;
 };
 }

src/gui/newwizard/setupwizardcontext.cpp
@@ -1,0 +1,62 @@
+/*
+ * Copyright (C) Fabian Müller <fmueller@owncloud.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+#include "setupwizardcontext.h"
+
+namespace OCC::Wizard {
+
+SetupWizardContext::SetupWizardContext(QWidget *windowParent, QObject *parent)
+    : QObject(parent)
+    , _window(new SetupWizardWindow(windowParent))
+{
+    resetAccessManager();
+}
+
+SetupWizardContext::~SetupWizardContext()
+{
+    _window->deleteLater();
+    _accessManager->deleteLater();
+}
+
+AccessManager *SetupWizardContext::resetAccessManager()
+{
+    if (_accessManager != nullptr) {
+        _accessManager->deleteLater();
+    }
+
+    _accessManager = new AccessManager(this);
+    return _accessManager;
+}
+
+SetupWizardWindow *SetupWizardContext::window() const
+{
+    return _window;
+}
+
+SetupWizardAccountBuilder &SetupWizardContext::accountBuilder()
+{
+    return _accountBuilder;
+}
+
+AccessManager *SetupWizardContext::accessManager() const
+{
+    return _accessManager;
+}
+
+void SetupWizardContext::resetAccountBuilder()
+{
+    _accountBuilder = {};
+}
+
+} // OCC::Wizard

src/gui/newwizard/setupwizardcontext.h
@@ -1,0 +1,56 @@
+/*
+ * Copyright (C) Fabian Müller <fmueller@owncloud.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+#pragma once
+
+#include "accessmanager.h"
+#include "setupwizardwindow.h"
+#include <QtGlobal>
+
+namespace OCC::Wizard {
+
+/**
+ * This class makes sharing wizard related objects between the controller and the states easier.
+ * It also allows us to provide standardized
+ */
+class SetupWizardContext : public QObject
+{
+    Q_OBJECT
+
+    Q_DISABLE_COPY_MOVE(SetupWizardContext)
+
+public:
+    explicit SetupWizardContext(QWidget *windowParent, QObject *parent);
+    ~SetupWizardContext() override;
+
+    /**
+     * Delete old access manager and create a new one.
+     * @return
+     */
+    AccessManager *resetAccessManager();
+
+    SetupWizardWindow *window() const;
+
+    SetupWizardAccountBuilder &accountBuilder();
+    void resetAccountBuilder();
+
+    AccessManager *accessManager() const;
+
+private:
+    SetupWizardWindow *_window = nullptr;
+    AccessManager *_accessManager = nullptr;
+    SetupWizardAccountBuilder _accountBuilder;
+};
+
+} // OCC::Wizard

src/gui/newwizard/setupwizardcontroller.h
@@ -17,7 +17,9 @@
 #include "account.h"
 #include "pages/abstractsetupwizardpage.h"
 #include "setupwizardaccountbuilder.h"
+#include "setupwizardcontext.h"
 #include "setupwizardwindow.h"
+#include "states/abstractsetupwizardstate.h"
 #include "syncmode.h"
 
 #include <QDialog>
@@ -50,16 +52,12 @@ Q_SIGNALS:
     void finished(AccountPtr newAccount, SyncMode syncMode);
 
 private:
-    void nextStep(std::optional<PageIndex> currentPage, std::optional<PageIndex> userName);
+    void nextStep(std::optional<PageIndex> desiredPage);
 
-    SetupWizardWindow *_wizardWindow;
+    SetupWizardContext *_context = nullptr;
 
     // keeping a pointer on the current page allows us to check whether the controller has been initialized yet
     // the pointer is also used to clean up the page
-    QPointer<AbstractSetupWizardPage> _currentPage;
-
-    SetupWizardAccountBuilder _accountBuilder;
-
-    AccessManager *_accessManager;
+    AbstractSetupWizardState *_currentState = nullptr;
 };
 }

commit 6d8fc63c874acc3f64779fedf8a484ed13bab07e

Fix regression introduced in b479539d9b08c56d0256c58b58372ab0dbefccd1

src/gui/newwizard/states/oauthcredentialssetupwizardstate.cpp
@@ -29,7 +29,7 @@ OAuthCredentialsSetupWizardState::OAuthCredentialsSetupWizardState(SetupWizardCo
     // username might not be set yet, shouldn't matter, though
     auto oAuth = new OAuth(_context->accountBuilder().serverUrl(), QString(), _context->accessManager(), {}, this);
 
-    connect(oAuth, &OAuth::result, this, [this, oAuthCredentialsPage](OAuth::Result result, const QString &userName, const QString &token, const QString &displayName, const QString &refreshToken) {
+    connect(oAuth, &OAuth::result, this, [this, oAuthCredentialsPage](OAuth::Result result, const QString &userName, const QString &token, const QString &refreshToken, const QString &displayName) {
         // the button may not be clicked anymore, since the server has been shut down right before this signal was emitted by the OAuth instance
         oAuthCredentialsPage->disableButtons();
 