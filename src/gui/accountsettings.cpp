restot/opencloud-desktop

commit 78dd638aad9a90907edb923894154aac33628c86

Fix crash
Fixes: #9600

src/gui/folderstatusmodel.cpp
@@ -553,6 +553,9 @@ bool FolderStatusModel::canFetchMore(const QModelIndex &parent) const
 
 void FolderStatusModel::fetchMore(const QModelIndex &parent)
 {
+    if (!data(parent, FolderStatusDelegate::IsReady).toBool()) {
+        return;
+    }
     auto info = infoForIndex(parent);
 
     if (!info || info->_fetched || info->_fetchingJob)

src/gui/folder.cpp
@@ -599,6 +599,7 @@ int Folder::slotWipeErrorBlacklist()
 
 void Folder::slotWatchedPathChanged(const QString &path, ChangeReason reason)
 {
+    Q_ASSERT(isReady());
     if (!FileSystem::isChildPathOf(path, this->path())) {
         qCDebug(lcFolder) << "Changed path is not contained in folder, ignoring:" << path;
         return;
@@ -809,22 +810,25 @@ void Folder::removeFromSettings() const
 
 bool Folder::isFileExcludedAbsolute(const QString &fullPath) const
 {
+    if (!_engine) {
+        return true;
+    }
     return _engine->excludedFiles().isExcluded(fullPath, path(), _definition.ignoreHiddenFiles);
 }
 
 bool Folder::isFileExcludedRelative(const QString &relativePath) const
 {
-    return _engine->excludedFiles().isExcluded(path() + relativePath, path(), _definition.ignoreHiddenFiles);
+    return isFileExcludedAbsolute(path() + relativePath);
 }
 
 void Folder::slotTerminateSync()
 {
-    qCInfo(lcFolder) << "folder " << alias() << " Terminating!";
-
-    if (_engine->isSyncRunning()) {
-        _engine->abort();
-
-        setSyncState(SyncResult::SyncAbortRequested);
+    if (isReady()) {
+        qCInfo(lcFolder) << "folder " << alias() << " Terminating!";
+        if (_engine->isSyncRunning()) {
+            _engine->abort();
+            setSyncState(SyncResult::SyncAbortRequested);
+        }
     }
 }
 
@@ -875,6 +879,9 @@ void Folder::wipeForRemoval()
 
 bool Folder::reloadExcludes()
 {
+    if (!_engine) {
+        return true;
+    }
     return _engine->excludedFiles().reloadExcludeFiles();
 }
 
@@ -941,6 +948,7 @@ void Folder::startSync()
 
 void Folder::setSyncOptions()
 {
+    Q_ASSERT(isReady());
     SyncOptions opt;
     ConfigFile cfgFile;
 
@@ -964,6 +972,7 @@ void Folder::setSyncOptions()
 
 void Folder::setDirtyNetworkLimits()
 {
+    Q_ASSERT(isReady());
     ConfigFile cfg;
     int downloadLimit = -75; // 75%
     int useDownLimit = cfg.useDownloadLimit();
@@ -999,6 +1008,7 @@ void Folder::slotSyncStarted()
 
 void Folder::slotSyncFinished(bool success)
 {
+    Q_ASSERT(isReady());
     qCInfo(lcFolder) << "Client version" << Theme::instance()->aboutVersions(Theme::VersionFormat::OneLiner);
 
     bool syncError = !_syncResult.errorStrings().isEmpty();

src/gui/folder.h
@@ -230,8 +230,16 @@ public:
     void setIgnoreHiddenFiles(bool ignore);
 
     // Used by the Socket API
-    SyncJournalDb *journalDb() { return &_journal; }
-    SyncEngine &syncEngine() { return *_engine; }
+    SyncJournalDb *journalDb()
+    {
+        Q_ASSERT(isReady());
+        return &_journal;
+    }
+    SyncEngine &syncEngine()
+    {
+        Q_ASSERT(isReady());
+        return *_engine;
+    }
     Vfs &vfs()
     {
         OC_ENFORCE(_vfs);

src/gui/ignorelisteditor.cpp
@@ -120,10 +120,12 @@ void IgnoreListEditor::slotUpdateLocalIgnoreList()
     // Otherwise we would not download the files/directories that are no longer
     // ignored (because the remote etag did not change)   (issue #3172)
     for (auto *folder : folderMan->map()) {
-        folder->journalDb()->forceRemoteDiscoveryNextSync();
-        folder->reloadExcludes();
-        folder->slotNextSyncFullLocalDiscovery();
-        folderMan->scheduleFolder(folder);
+        if (folder->isReady()) {
+            folder->journalDb()->forceRemoteDiscoveryNextSync();
+            folder->reloadExcludes();
+            folder->slotNextSyncFullLocalDiscovery();
+            folderMan->scheduleFolder(folder);
+        }
     }
 }
 

commit 037c5ccc158b6707b369d91f997059e8f0411bb6

Fix expansion of selective sync tree (#10058)
Fixes a bug introduced in 78dd638aa.

src/gui/accountsettings.cpp
@@ -258,9 +258,8 @@ void AccountSettings::slotCustomContextMenuRequested(const QPoint &pos)
         return;
     }
 
-
     // Only allow removal if the item isn't in "ready" state.
-    if (classification == FolderStatusModel::RootFolder && !_model->data(index, FolderStatusDelegate::IsReady).toBool() && !_model->folder(index)->isDeployed()) {
+    if (classification == FolderStatusModel::RootFolder && _model->folder(index)->isReady() && !_model->folder(index)->isDeployed()) {
         QMenu *menu = new QMenu(tv);
         menu->setAttribute(Qt::WA_DeleteOnClose);
         removeFolderAction(menu);

src/gui/folderstatusdelegate.h
@@ -48,8 +48,6 @@ public:
 
         AddButton, // 1 = enabled; 2 = disabled
         FolderSyncText,
-        
-        IsReady // boolean
     };
     void paint(QPainter *, const QStyleOptionViewItem &, const QModelIndex &) const override;
     QSize sizeHint(const QStyleOptionViewItem &, const QModelIndex &) const override;

src/gui/folderstatusmodel.cpp
@@ -281,8 +281,6 @@ QVariant FolderStatusModel::data(const QModelIndex &index, int role) const
         return progress._overallSyncString;
     case FolderStatusDelegate::FolderSyncText:
         return tr("Local folder: %1").arg(f->shortGuiLocalPath());
-    case FolderStatusDelegate::IsReady:
-        return f->isReady();
     }
     return QVariant();
 }
@@ -570,9 +568,10 @@ bool FolderStatusModel::canFetchMore(const QModelIndex &parent) const
 
 void FolderStatusModel::fetchMore(const QModelIndex &parent)
 {
-    if (!data(parent, FolderStatusDelegate::IsReady).toBool()) {
+    if (!folder(parent)->isReady()) {
         return;
     }
+
     auto info = infoForIndex(parent);
 
     if (!info || info->_fetched || info->_fetchingJob)