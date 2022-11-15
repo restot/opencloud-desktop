restot/opencloud-desktop

commit e86b320e1fcc102cefca7bb14d43590a2be06bf9

Remove cdefines

src/csync/vio/csync_vio_local_unix.cpp
@@ -26,8 +26,6 @@
 #include <dirent.h>
 #include <stdio.h>
 
-#include "c_private.h"
-#include "c_lib.h"
 #include "csync.h"
 
 #include "vio/csync_vio_local.h"
@@ -47,14 +45,14 @@ struct csync_vio_handle_t {
   QByteArray path;
 };
 
-static int _csync_vio_local_stat_mb(const mbchar_t *wuri, csync_file_stat_t *buf);
+static int _csync_vio_local_stat_mb(const char *wuri, csync_file_stat_t *buf);
 
 csync_vio_handle_t *csync_vio_local_opendir(const QString &name) {
     QScopedPointer<csync_vio_handle_t> handle(new csync_vio_handle_t{});
 
     auto dirname = QFile::encodeName(name);
 
-    handle->dh = _topendir(dirname.constData());
+    handle->dh = opendir(dirname.constData());
     if (!handle->dh) {
         return nullptr;
     }
@@ -65,21 +63,20 @@ csync_vio_handle_t *csync_vio_local_opendir(const QString &name) {
 
 int csync_vio_local_closedir(csync_vio_handle_t *dhandle) {
     Q_ASSERT(dhandle);
-    auto rc = _tclosedir(dhandle->dh);
+    auto rc = closedir(dhandle->dh);
     delete dhandle;
     return rc;
 }
 
 std::unique_ptr<csync_file_stat_t> csync_vio_local_readdir(csync_vio_handle_t *handle, OCC::Vfs *vfs) {
+    struct dirent *dirent = nullptr;
+    std::unique_ptr<csync_file_stat_t> file_stat;
 
-  struct _tdirent *dirent = nullptr;
-  std::unique_ptr<csync_file_stat_t> file_stat;
-
-  do {
-      dirent = _treaddir(handle->dh);
-      if (dirent == nullptr)
-          return {};
-  } while (qstrcmp(dirent->d_name, ".") == 0 || qstrcmp(dirent->d_name, "..") == 0);
+    do {
+        dirent = readdir(handle->dh);
+        if (dirent == nullptr)
+            return {};
+    } while (qstrcmp(dirent->d_name, ".") == 0 || qstrcmp(dirent->d_name, "..") == 0);
 
   file_stat.reset(new csync_file_stat_t);
   file_stat->path = QFile::decodeName(dirent->d_name);
@@ -130,11 +127,11 @@ int csync_vio_local_stat(const QString &uri, csync_file_stat_t *buf)
     return _csync_vio_local_stat_mb(QFile::encodeName(uri).constData(), buf);
 }
 
-static int _csync_vio_local_stat_mb(const mbchar_t *wuri, csync_file_stat_t *buf)
+static int _csync_vio_local_stat_mb(const char *wuri, csync_file_stat_t *buf)
 {
-    csync_stat_t sb;
+    struct stat sb;
 
-    if (_tstat(wuri, &sb) < 0) {
+    if (stat(wuri, &sb) < 0) {
         return -1;
     }
 

src/csync/csync.h
@@ -32,7 +32,6 @@
 #ifndef _CSYNC_H
 #define _CSYNC_H
 
-#include "std/c_private.h"
 #include "ocsynclib.h"
 
 #include <sys/stat.h>

src/csync/csync_exclude.cpp
@@ -24,10 +24,6 @@
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
 #endif
-
-#include "c_lib.h"
-#include "c_private.h"
-
 #include "csync_exclude.h"
 
 #include "common/filesystembase.h"

src/csync/std/c_lib.h
@@ -1,24 +1,0 @@
-/*
- * cynapses libc functions
- *
- * Copyright (c) 2008-2013 by Andreas Schneider <asn@cryptomilk.org>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- */
-
-#include <stdlib.h>
-#include <string.h>
-
-#include "c_private.h"

src/csync/std/c_private.h
@@ -1,151 +1,0 @@
-/*
- * cynapses libc functions
- *
- * Copyright (c) 2008-2013 by Andreas Schneider <asn@cryptomilk.org>
- * Copyright (c) 2012-2013 by Dominik Schmidt <dev@dominik-schmidt.de>
- * Copyright (c) 2012-2013 by Klaas Freitag <freitag@owncloud.com>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- */
-
-#ifndef _C_PRIVATE_H
-#define _C_PRIVATE_H
-
-#include "config_csync.h"
-
-/* cross platform defines */
-#include "config_csync.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-
-#ifdef _WIN32
-#include <windows.h>
-#include <windef.h>
-#include <winbase.h>
-#include <wchar.h>
-#else
-#include <unistd.h>
-#endif
-
-#include <errno.h>
-
-#ifdef __MINGW32__
-#ifndef S_IRGRP
-#define S_IRGRP 0
-#endif
-#ifndef S_IROTH
-#define S_IROTH 0
-#endif
-#ifndef S_IXGRP
-#define S_IXGRP 0
-#endif
-#ifndef S_IXOTH
-#define S_IXOTH 0
-#endif
-
-#define S_IFSOCK 10000 /* dummy val on Win32 */
-#define S_IFLNK 10001  /* dummy val on Win32 */
-
-#define O_NOFOLLOW 0
-#define O_NOCTTY 0
-
-#define uid_t int
-#define gid_t int
-#define nlink_t int
-#define getuid() 0
-#define geteuid() 0
-#elif defined(_WIN32)
-#define mode_t int
-#else
-#include <fcntl.h>
-#endif
-
-
-#ifdef _WIN32
-typedef struct stat64 csync_stat_t;
-#define _FILE_OFFSET_BITS 64
-#else
-typedef struct stat csync_stat_t;
-#endif
-
-#ifndef O_NOATIME
-#define O_NOATIME 0
-#endif
-
-#ifndef HAVE_LSTAT
-#define lstat _stat
-#endif
-
-/* tchar definitions for clean win32 filenames */
-#ifndef _UNICODE
-#define _UNICODE
-#endif
-
-#if defined _WIN32 && defined _UNICODE
-typedef  wchar_t         mbchar_t;
-#define _topen           _wopen
-#define _tdirent         _wdirent
-#define _topendir        _wopendir
-#define _tclosedir       _wclosedir
-#define _treaddir        _wreaddir
-#define _trewinddir      _wrewinddir
-#define _ttelldir        _wtelldir
-#define _tseekdir        _wseekdir
-#define _tcreat          _wcreat
-#define _tstat           _wstat64
-#define _tfstat          _fstat64
-#define _tunlink         _wunlink
-#define _tmkdir(X,Y)     _wmkdir(X)
-#define _trmdir	         _wrmdir
-#define _tchmod          _wchmod
-#define _trewinddir      _wrewinddir
-#define _tchown(X, Y, Z)  0 /* no chown on Win32 */
-#define _tchdir          _wchdir
-#define _tgetcwd         _wgetcwd
-#else
-typedef char           mbchar_t;
-#define _tdirent       dirent
-#define _topen         open
-#define _topendir      opendir
-#define _tclosedir     closedir
-#define _treaddir      readdir
-#define _trewinddir    rewinddir
-#define _ttelldir      telldir
-#define _tseekdir      seekdir
-#define _tcreat        creat
-#define _tstat         lstat
-#define _tfstat        fstat
-#define _tunlink       unlink
-#define _tmkdir(X,Y)   mkdir(X,Y)
-#define _trmdir	       rmdir
-#define _tchmod        chmod
-#define _trewinddir    rewinddir
-#define _tchown(X,Y,Z) chown(X,Y,Z)
-#define _tchdir        chdir
-#define _tgetcwd       getcwd
-#endif
-
-/* FIXME: Implement TLS for OS X */
-#if defined(__GNUC__) && !defined(__APPLE__)
-# define CSYNC_THREAD __thread
-#elif defined(_MSC_VER)
-# define CSYNC_THREAD __declspec(thread)
-#else
-# define CSYNC_THREAD
-#endif
-
-#endif //_C_PRIVATE_H
-
-/* vim: set ft=c.doxygen ts=8 sw=2 et cindent: */

src/csync/std/c_time.cpp
@@ -19,7 +19,6 @@
  */
 
 #include "config_csync.h"
-#include "c_private.h"
 #include "c_time.h"
 
 #include "common/filesystembase.h"

src/csync/vio/csync_vio_local_win.cpp
@@ -27,8 +27,6 @@
 
 #include "windows.h"
 
-#include "c_private.h"
-#include "c_lib.h"
 #include "csync.h"
 #include "vio/csync_vio_local.h"
 #include "common/filesystembase.h"

src/gui/folderwatcher_linux.cpp
@@ -16,6 +16,7 @@
 
 #include <cerrno>
 #include <sys/inotify.h>
+#include <unistd.h>
 
 #include "folder.h"
 #include "folderwatcher_linux.h"

commit c46d91b6e2d1a8bed05adda2b1bcd550ac50c034

Fix symlink detection on Unix
Regression was introduced in: e86b320e1fcc102cefca7bb14d43590a2be06bf9

src/csync/vio/csync_vio_local_unix.cpp
@@ -120,7 +120,7 @@ int csync_vio_local_stat(const QString &uri, csync_file_stat_t *buf)
 {
     struct stat sb;
 
-    if (stat(QFile::encodeName(uri).constData(), &sb) < 0) {
+    if (lstat(QFile::encodeName(uri).constData(), &sb) < 0) {
         return -1;
     }
 