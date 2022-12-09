restot/opencloud-desktop

commit 76f536eebd568c3652d10fa192308f9e75064d57

Make the CheckServer job independent of the account
The job needs to run on an independent QNAM

src/gui/connectionvalidator.cpp
@@ -24,6 +24,7 @@
 #include "connectionvalidator.h"
 #include "creds/abstractcredentials.h"
 #include "networkjobs.h"
+#include "networkjobs/checkserverjobfactory.h"
 #include "networkjobs/jsonjob.h"
 #include "theme.h"
 #include "tlserrordialog.h"
@@ -96,19 +97,43 @@ void ConnectionValidator::slotCheckServerAndAuth()
 {
     // ensure we receive ssl errors
     qDebug() << "reset";
-    //_account->accessManager()->clearConnectionCache();
-    CheckServerJob *checkJob = new CheckServerJob(_account, this);
-    checkJob->setClearCookies(_clearCookies);
-    checkJob->setTimeout(timeoutToUse);
-    connect(checkJob, &CheckServerJob::instanceFound, this, &ConnectionValidator::slotStatusFound);
-    connect(checkJob, &CheckServerJob::instanceNotFound, this, &ConnectionValidator::slotNoStatusFound);
-    connect(checkJob, &CheckServerJob::timeout, this, [checkJob, this] {
-        qCWarning(lcConnectionValidator) << checkJob;
-        _errors.append(tr("timeout"));
-        reportResult(Timeout);
+
+    auto checkServerJob = CheckServerJobFactory(_account->accessManager()).startJob(_account->url());
+
+    // FIXME: sslErrors() signal not available
+    connect(checkServerJob, &CoreJob::finished, this, [checkServerJob, this]() {
+        if (checkServerJob->success()) {
+            auto result = checkServerJob->result().value<CheckServerJobResult>();
+            slotStatusFound(result.serverUrl(), result.statusObject());
+        } else {
+            qCWarning(lcConnectionValidator) << checkServerJob->reply()->error() << checkServerJob << checkServerJob->reply()->peek(1024);
+
+            switch (checkServerJob->reply()->error()) {
+            case QNetworkReply::TimeoutError:
+                qCWarning(lcConnectionValidator) << checkServerJob;
+                _errors.append(tr("timeout"));
+                reportResult(Timeout);
+                return;
+            case QNetworkReply::SslHandshakeFailedError:
+                reportResult(SslError);
+                return;
+            case QNetworkReply::TooManyRedirectsError:
+                reportResult(MaintenanceMode);
+                return;
+            default:
+                break;
+            }
+
+            if (!_account->credentials()->stillValid(checkServerJob->reply())) {
+                // Note: Why would this happen on a status.php request?
+                _errors.append(tr("Authentication error: Either username or password are wrong."));
+            } else {
+                //_errors.append(tr("Unable to connect to %1").arg(_account->url().toString()));
+                _errors.append(checkServerJob->errorMessage());
+            }
+            reportResult(StatusNotFound);
+        }
     });
-    connect(checkJob, &CheckServerJob::sslErrors, this, &ConnectionValidator::sslErrors);
-    checkJob->start();
 }
 
 void ConnectionValidator::slotStatusFound(const QUrl &url, const QJsonObject &info)
@@ -116,12 +141,12 @@ void ConnectionValidator::slotStatusFound(const QUrl &url, const QJsonObject &in
     // Newer servers don't disclose any version in status.php anymore
     // https://github.com/owncloud/core/pull/27473/files
     // so this string can be empty.
-    QString serverVersion = CheckServerJob::version(info);
+    QString serverVersion = info.value(QLatin1String("version")).toString() + QLatin1Char('-') + info.value(QLatin1String("productname")).toString();
 
     // status.php was found.
     qCInfo(lcConnectionValidator) << "** Application: ownCloud found: "
                                   << url << " with version "
-                                  << CheckServerJob::versionString(info)
+                                  << info.value(QLatin1String("versionstring")).toString()
                                   << "(" << serverVersion << ")";
 
     // Update server url in case of redirection
@@ -149,27 +174,6 @@ void ConnectionValidator::slotStatusFound(const QUrl &url, const QJsonObject &in
     } else {
         reportResult(Connected);
     }
-}
-
-// status.php could not be loaded (network or server issue!).
-void ConnectionValidator::slotNoStatusFound(QNetworkReply *reply)
-{
-    auto job = qobject_cast<CheckServerJob *>(sender());
-    qCWarning(lcConnectionValidator) << reply->error() << job << reply->peek(1024);
-    if (reply->error() == QNetworkReply::SslHandshakeFailedError) {
-        reportResult(SslError);
-        return;
-    } else if (reply->error() == QNetworkReply::TooManyRedirectsError) {
-        reportResult(MaintenanceMode);
-        return;
-    } else if (!_account->credentials()->stillValid(reply)) {
-        // Note: Why would this happen on a status.php request?
-        _errors.append(tr("Authentication error: Either username or password are wrong."));
-    } else {
-        //_errors.append(tr("Unable to connect to %1").arg(_account->url().toString()));
-        _errors.append(job->errorString());
-    }
-    reportResult(StatusNotFound);
 }
 
 void ConnectionValidator::checkAuthentication()

src/libsync/creds/oauth.cpp
@@ -20,6 +20,7 @@
 #include "creds/httpcredentials.h"
 #include "creds/jobs/determineuserjobfactory.h"
 #include "networkjobs.h"
+#include "networkjobs/checkserverjobfactory.h"
 #include "theme.h"
 
 #include <QApplication>
@@ -597,23 +598,19 @@ void AccountBasedOAuth::startAuthentication()
 
 void AccountBasedOAuth::fetchWellKnown()
 {
-    auto *checkServerJob = new CheckServerJob(_account->sharedFromThis(), this);
-    checkServerJob->setClearCookies(true);
-    checkServerJob->setTimeout(defaultTimeout());
+    auto *checkServerJob = CheckServerJobFactory(_networkAccessManager, this).startJob(_serverUrl);
 
-    connect(checkServerJob, &CheckServerJob::instanceNotFound, this, [this](QNetworkReply *reply) {
-        if (_isRefreshingToken) {
-            Q_EMIT refreshError(reply->error(), reply->errorString());
+    connect(checkServerJob, &CoreJob::finished, this, [checkServerJob, this]() {
+        if (checkServerJob->success()) {
+            OAuth::fetchWellKnown();
         } else {
-            Q_EMIT result(Error);
+            if (_isRefreshingToken) {
+                Q_EMIT refreshError(checkServerJob->reply()->error(), checkServerJob->errorMessage());
+            } else {
+                Q_EMIT result(Error);
+            }
         }
     });
-
-    connect(checkServerJob, &CheckServerJob::instanceFound, this, [this](const QUrl &url, const QJsonObject &info) {
-        OAuth::fetchWellKnown();
-    });
-
-    checkServerJob->start();
 }
 
 void AccountBasedOAuth::dynamicRegistrationDataReceived(const QVariantMap &dynamicRegistrationData)

src/libsync/networkjobs/checkserverjobfactory.cpp
@@ -1,0 +1,139 @@
+/*
+ * Copyright (C) Fabian Müller <fmueller@owncloud.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+#include "checkserverjobfactory.h"
+#include "common/utility.h"
+#include "creds/httpcredentials.h"
+#include <QJsonParseError>
+
+namespace {
+
+// FIXME: this is not a permanent solution, eventually we want to replace the job factories with job classes so we can store such information there
+class CheckServerCoreJob : OCC::CoreJob
+{
+    friend OCC::CheckServerJobFactory;
+
+private:
+    // doesn't concern users of the job factory
+    // we just need a place to maintain these variables, but the factory is likely deleted before the job has finished
+    bool _redirectDistinct;
+    bool _firstTry;
+};
+
+}
+
+namespace OCC {
+
+Q_LOGGING_CATEGORY(lcCheckServerJob, "sync.checkserverjob", QtInfoMsg)
+
+CheckServerJobResult::CheckServerJobResult(const QJsonObject &statusObject, const QUrl &serverUrl)
+    : _statusObject(statusObject)
+    , _serverUrl(serverUrl)
+{
+}
+
+QJsonObject CheckServerJobResult::statusObject() const
+{
+    return _statusObject;
+}
+
+QUrl CheckServerJobResult::serverUrl() const
+{
+    return _serverUrl;
+}
+
+CoreJob *CheckServerJobFactory::startJob(const QUrl &url)
+{
+    // the custom job class is used to store some state we need to maintain until the job has finished
+    auto job = new CheckServerCoreJob;
+
+    auto req = makeRequest(Utility::concatUrlPath(url, QStringLiteral("status.php")));
+
+    req.setAttribute(QNetworkRequest::RedirectPolicyAttribute, QNetworkRequest::NoLessSafeRedirectPolicy);
+    req.setRawHeader(QByteArrayLiteral("OC-Connection-Validator"), QByteArrayLiteral("desktop"));
+    req.setMaximumRedirectsAllowed(_maxRedirectsAllowed);
+
+    auto *reply = nam()->get(req);
+
+    connect(reply, &QNetworkReply::redirected, job, [reply, job] {
+        const auto code = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+        if (code == 302 || code == 307) {
+            job->_redirectDistinct = false;
+        }
+    });
+
+    connect(reply, &QNetworkReply::finished, job, [url, reply, job] {
+        reply->deleteLater();
+
+        // need a mutable copy
+        auto serverUrl = url;
+
+        const QUrl targetUrl = reply->url().adjusted(QUrl::RemoveFilename);
+
+        // TODO: still needed?
+        if (targetUrl.scheme() == QLatin1String("https")
+            && reply->sslConfiguration().sessionTicket().isEmpty()
+            && reply->error() == QNetworkReply::NoError) {
+            qCWarning(lcCheckServerJob) << "No SSL session identifier / session ticket is used, this might impact sync performance negatively.";
+        }
+
+        if (serverUrl != targetUrl) {
+            if (job->_redirectDistinct) {
+                serverUrl = targetUrl;
+            } else {
+                if (job->_firstTry) {
+                    qCWarning(lcCheckServerJob) << "Server might have moved, retry";
+                    job->_firstTry = false;
+                    job->_redirectDistinct = true;
+
+                    // FIXME
+                } else {
+                    qCWarning(lcCheckServerJob) << "We got a temporary moved server aborting";
+                    setJobError(job, QStringLiteral("Illegal redirect by server"), reply);
+                }
+            }
+        }
+
+        const int httpStatus = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+        if (reply->error() == QNetworkReply::TooManyRedirectsError) {
+            qCWarning(lcCheckServerJob) << "error:" << reply->errorString();
+            setJobError(job, reply->errorString(), reply);
+        } else if (httpStatus != 200 || reply->bytesAvailable() == 0) {
+            qCWarning(lcCheckServerJob) << "error: status.php replied " << httpStatus;
+            setJobError(job, QStringLiteral("Invalid HTTP status code received for status.php: %1").arg(httpStatus), reply);
+        } else {
+            const QByteArray body = reply->peek(4 * 1024);
+            QJsonParseError error;
+            auto status = QJsonDocument::fromJson(body, &error);
+            // empty or invalid response
+            if (error.error != QJsonParseError::NoError || status.isNull()) {
+                qCWarning(lcCheckServerJob) << "status.php from server is not valid JSON!" << body << reply->request().url() << error.errorString();
+            }
+
+            qCInfo(lcCheckServerJob) << "status.php returns: " << status << " " << reply->error() << " Reply: " << reply;
+
+            if (status.object().contains(QStringLiteral("installed"))) {
+                CheckServerJobResult result(status.object(), serverUrl);
+                setJobResult(job, QVariant::fromValue(result));
+            } else {
+                qCWarning(lcCheckServerJob) << "No proper answer on " << reply->url();
+                setJobError(job, QStringLiteral("Did not receive expected reply from server"), reply);
+            }
+        }
+    });
+
+    return job;
+}
+
+} // OCC

src/libsync/networkjobs/checkserverjobfactory.h
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) Fabian Müller <fmueller@owncloud.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+#pragma once
+
+#include "abstractcorejob.h"
+
+#include <QJsonObject>
+
+namespace OCC {
+
+class OWNCLOUDSYNC_EXPORT CheckServerJobResult
+{
+
+public:
+    CheckServerJobResult() = default;
+    CheckServerJobResult(const QJsonObject &statusObject, const QUrl &serverUrl);
+
+    QJsonObject statusObject() const;
+    QUrl serverUrl() const;
+
+private:
+    const QJsonObject _statusObject;
+    const QUrl _serverUrl;
+};
+
+
+class OWNCLOUDSYNC_EXPORT CheckServerJobFactory : public AbstractCoreJobFactory
+{
+    Q_OBJECT
+
+public:
+    using AbstractCoreJobFactory::AbstractCoreJobFactory;
+
+    CoreJob *startJob(const QUrl &url) override;
+
+private:
+    int _maxRedirectsAllowed = 5;
+};
+
+} // OCC
+
+Q_DECLARE_METATYPE(OCC::CheckServerJobResult)

src/cmd/cmd.cpp
@@ -36,6 +36,7 @@
 #include "common/syncjournaldb.h"
 #include "config.h"
 #include "csync_exclude.h"
+#include "networkjobs/checkserverjobfactory.h"
 #include "networkjobs/jsonjob.h"
 #include "syncengine.h"
 
@@ -553,44 +554,51 @@ int main(int argc, char **argv)
 
     ctx.account->setUrl(ctx.credentialFreeUrl);
 
-    auto *checkServer = new CheckServerJob(ctx.account, nullptr);
-    QObject::connect(checkServer, &CheckServerJob::instanceFound, [ctx] {
-        // Perform a call to get the capabilities.
-        auto *capabilitiesJob = new JsonApiJob(ctx.account, QStringLiteral("ocs/v1.php/cloud/capabilities"), {}, {}, nullptr);
-        QObject::connect(capabilitiesJob, &JsonApiJob::finishedSignal, qApp, [capabilitiesJob, ctx] {
-            auto caps = capabilitiesJob->data().value("ocs").toObject().value("data").toObject().value("capabilities").toObject();
-            qDebug() << "Server capabilities" << caps;
-            ctx.account->setCapabilities(caps.toVariantMap());
-            ctx.account->setServerVersion(caps["core"].toObject()["status"].toObject()["version"].toString());
+    auto *checkServerJob = CheckServerJobFactory(ctx.account->accessManager()).startJob(ctx.account->url());
 
-            if (capabilitiesJob->reply()->error() != QNetworkReply::NoError) {
-                qFatal("Error connecting to server");
-            }
+    QObject::connect(checkServerJob, &CoreJob::finished, [ctx, checkServerJob] {
+        if (checkServerJob->success()) {
+            // Perform a call to get the capabilities.
+            auto *capabilitiesJob = new JsonApiJob(ctx.account, QStringLiteral("ocs/v1.php/cloud/capabilities"), {}, {}, nullptr);
+            QObject::connect(capabilitiesJob, &JsonApiJob::finishedSignal, qApp, [capabilitiesJob, ctx] {
+                auto caps = capabilitiesJob->data().value("ocs").toObject().value("data").toObject().value("capabilities").toObject();
+                qDebug() << "Server capabilities" << caps;
+                ctx.account->setCapabilities(caps.toVariantMap());
+                ctx.account->setServerVersion(caps["core"].toObject()["status"].toObject()["version"].toString());
 
-            auto userJob = new JsonApiJob(ctx.account, QLatin1String("ocs/v1.php/cloud/user"), {}, {}, nullptr);
-            QObject::connect(userJob, &JsonApiJob::finishedSignal, qApp, [userJob, ctx] {
-                const QJsonObject data = userJob->data().value("ocs").toObject().value("data").toObject();
-                ctx.account->setDavUser(data.value("id").toString());
-                ctx.account->setDavDisplayName(data.value("display-name").toString());
+                if (capabilitiesJob->reply()->error() != QNetworkReply::NoError) {
+                    qFatal("Error connecting to server");
+                }
 
-                // much lower age than the default since this utility is usually made to be run right after a change in the tests
-                SyncEngine::minimumFileAgeForUpload = std::chrono::milliseconds(0);
-                sync(ctx);
+                auto userJob = new JsonApiJob(ctx.account, QLatin1String("ocs/v1.php/cloud/user"), {}, {}, nullptr);
+                QObject::connect(userJob, &JsonApiJob::finishedSignal, qApp, [userJob, ctx] {
+                    const QJsonObject data = userJob->data().value("ocs").toObject().value("data").toObject();
+                    ctx.account->setDavUser(data.value("id").toString());
+                    ctx.account->setDavDisplayName(data.value("display-name").toString());
+
+                    // much lower age than the default since this utility is usually made to be run right after a change in the tests
+                    SyncEngine::minimumFileAgeForUpload = std::chrono::milliseconds(0);
+                    sync(ctx);
+                });
+                userJob->start();
             });
-            userJob->start();
-        });
-        capabilitiesJob->start();
-    });
-    QObject::connect(checkServer, &CheckServerJob::instanceNotFound, [ctx] {
-        qFatal("Failed to resolve %s.", qPrintable(ctx.account->url().toString()));
-    });
-    QObject::connect(checkServer, &CheckServerJob::timedOut, [ctx] {
-        qFatal("Looking up %s timed out.", qPrintable(ctx.account->url().toString()));
-    });
-    QObject::connect(checkServer, &CheckServerJob::sslErrors, [ctx] {
-        qFatal(APPLICATION_EXECUTABLE "cmd currently does not support untrusted ssl certificates.");
+            capabilitiesJob->start();
+        } else {
+            switch (checkServerJob->reply()->error()) {
+            case QNetworkReply::OperationCanceledError:
+                qFatal("Looking up %s timed out.", qPrintable(ctx.account->url().toString()));
+                break;
+            default:
+                qFatal("Failed to resolve %s.", qPrintable(ctx.account->url().toString()));
+            }
+        }
     });
 
-    checkServer->start();
+    // FIXME: not supported any more
+    // the job should never run into this, though
+    //    QObject::connect(checkServer, &CheckServerJob::sslErrors, [ctx] {
+    //        qFatal(APPLICATION_EXECUTABLE "cmd currently does not support untrusted ssl certificates.");
+    //    });
+
     return app.exec();
 }

src/gui/connectionvalidator.h
@@ -125,7 +125,6 @@ protected slots:
     void slotCheckServerAndAuth();
 
     void slotStatusFound(const QUrl &url, const QJsonObject &info);
-    void slotNoStatusFound(QNetworkReply *reply);
 
     void slotAuthFailed(QNetworkReply *reply);
     void slotAuthSuccess();

src/libsync/CMakeLists.txt
@@ -52,6 +52,7 @@ set(libsync_SRCS
     creds/credentialscommon.cpp
     creds/oauth.cpp
 
+    networkjobs/checkserverjobfactory.cpp
     networkjobs/jsonjob.cpp
 
     abstractcorejob.cpp

commit e5c75d6f0ab720ab8aa8d48dfab674e955520e46

Restore pre 3.0 behaviour of clearing the cookies befor a token refresh (#10338)
This fixes a regression introduced in:
76f536eebd568c3652d10fa192308f9e75064d57
20f5e7fe90a4606e34cfef4423fad846401e09fb

src/gui/connectionvalidator.cpp
@@ -97,30 +97,20 @@ void ConnectionValidator::systemProxyLookupDone(const QNetworkProxy &proxy)
 // The actual check
 void ConnectionValidator::slotCheckServerAndAuth()
 {
-    // in order to receive all ssl erorrs we need a fresh QNam
-    auto nam = _account->credentials()->createAM();
-    nam->setCustomTrustedCaCertificates(_account->approvedCerts());
+    auto checkServerFactory = CheckServerJobFactory::createFromAccount(_account, _clearCookies, this);
+    auto checkServerJob = checkServerFactory.startJob(_account->url(), this);
 
-    // do we start with the old cookies or new
-    if (!_clearCookies) {
-        const auto accountCookies = _account->accessManager()->ownCloudCookieJar()->allCookies();
-        nam->ownCloudCookieJar()->setAllCookies(accountCookies);
-    }
-
-    auto checkServerJob = CheckServerJobFactory(nam).startJob(_account->url(), this);
-
-    connect(nam, &AccessManager::sslErrors, this, [this](QNetworkReply *reply, const QList<QSslError> &errors) {
+    connect(checkServerJob->reply()->manager(), &AccessManager::sslErrors, this, [this](QNetworkReply *reply, const QList<QSslError> &errors) {
         Q_UNUSED(reply)
         Q_EMIT sslErrors(errors);
     });
 
-    connect(checkServerJob, &CoreJob::finished, this, [checkServerJob, nam, this]() {
-        nam->deleteLater();
+    connect(checkServerJob, &CoreJob::finished, this, [checkServerJob, this]() {
         if (checkServerJob->success()) {
             const auto result = checkServerJob->result().value<CheckServerJobResult>();
 
             // adopt the new cookies
-            _account->accessManager()->setCookieJar(nam->cookieJar());
+            _account->accessManager()->setCookieJar(checkServerJob->reply()->manager()->cookieJar());
 
             slotStatusFound(result.serverUrl(), result.statusObject());
         } else {

src/libsync/creds/oauth.cpp
@@ -607,7 +607,7 @@ void AccountBasedOAuth::fetchWellKnown()
 {
     qCDebug(lcOauth) << "starting CheckServerJob before fetching" << wellKnownPathC;
 
-    auto *checkServerJob = CheckServerJobFactory(_networkAccessManager).startJob(_serverUrl, this);
+    auto *checkServerJob = CheckServerJobFactory::createFromAccount(_account, true, this).startJob(_serverUrl, this);
 
     connect(checkServerJob, &CoreJob::finished, this, [checkServerJob, this]() {
         if (checkServerJob->success()) {

src/libsync/networkjobs/checkserverjobfactory.cpp
@@ -13,8 +13,14 @@
  */
 
 #include "checkserverjobfactory.h"
+
 #include "common/utility.h"
-#include "creds/httpcredentials.h"
+#include "libsync/accessmanager.h"
+#include "libsync/account.h"
+#include "libsync/cookiejar.h"
+#include "libsync/creds/httpcredentials.h"
+#include "libsync/theme.h"
+
 #include <QJsonParseError>
 
 namespace {
@@ -57,6 +63,20 @@ QJsonObject CheckServerJobResult::statusObject() const
 QUrl CheckServerJobResult::serverUrl() const
 {
     return _serverUrl;
+}
+
+CheckServerJobFactory CheckServerJobFactory::createFromAccount(const AccountPtr &account, bool clearCookies, QObject *parent)
+{
+    // in order to receive all ssl erorrs we need a fresh QNam
+    auto nam = account->credentials()->createAM();
+    nam->setCustomTrustedCaCertificates(account->approvedCerts());
+    nam->setParent(parent);
+    // do we start with the old cookies or new
+    if (!(clearCookies && Theme::instance()->connectionValidatorClearCookies())) {
+        const auto accountCookies = account->accessManager()->ownCloudCookieJar()->allCookies();
+        nam->ownCloudCookieJar()->setAllCookies(accountCookies);
+    }
+    return CheckServerJobFactory(nam);
 }
 
 CoreJob *CheckServerJobFactory::startJob(const QUrl &url, QObject *parent)

src/libsync/networkjobs/checkserverjobfactory.h
@@ -43,6 +43,12 @@ class OWNCLOUDSYNC_EXPORT CheckServerJobFactory : public AbstractCoreJobFactory
 public:
     using AbstractCoreJobFactory::AbstractCoreJobFactory;
 
+    /**
+     * clearCookies: Whether to clear the cookies before we start the CheckServerJob job
+     * This option also depends on Theme::instance()->connectionValidatorClearCookies()
+     */
+    static CheckServerJobFactory createFromAccount(const AccountPtr &account, bool clearCookies, QObject *parent);
+
     CoreJob *startJob(const QUrl &url, QObject *parent) override;
 
 private: