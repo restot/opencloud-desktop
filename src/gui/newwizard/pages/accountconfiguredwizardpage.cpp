restot/opencloud-desktop

commit f107781389c856b69b6a21bcbf828f9d821f218d

Don't deselect vfs if showVirtualFilesOption (#10479)

src/gui/newwizard/pages/accountconfiguredwizardpage.cpp
@@ -44,7 +44,6 @@ AccountConfiguredWizardPage::AccountConfiguredWizardPage(const QString &defaultS
 
     if (!Theme::instance()->showVirtualFilesOption()) {
         _ui->useVfsRadioButton->setVisible(false);
-        vfsIsAvailable = false;
     }
 
     if (!vfsIsAvailable) {

commit 8b94174accf9befcaa90ad3aca68487a11b39b38

Restore 2.11 behaviour for showVirtualFilesOption
This fixes a regression introduced in f107781389c856b69b6a21bcbf828f9d821f218d

src/gui/newwizard/pages/accountconfiguredwizardpage.cpp
@@ -44,6 +44,7 @@ AccountConfiguredWizardPage::AccountConfiguredWizardPage(const QString &defaultS
 
     if (!Theme::instance()->showVirtualFilesOption()) {
         _ui->useVfsRadioButton->setVisible(false);
+        enableVfsByDefault = false;
     }
 
     if (!vfsIsAvailable) {