7856b735a8766db1bcaf23d18c5eed782040f5bc
Hannah von Reth <h.vonreth@opencloud.eu>
2025-01-20 15:13:28 +0100

    Remove commented out vars

--- OPENCLOUD.cmake
+++ OPENCLOUD.cmake
@@ -3,9 +3,7 @@ set( APPLICATION_SHORTNAME  "OpenCloud" )
 set( APPLICATION_EXECUTABLE "opencloud" )
 set( APPLICATION_DOMAIN     "opencloud.eu" )
 set( APPLICATION_VENDOR     "OpenCloud" )
-#set( APPLICATION_UPDATE_URL "https://updates.owncloud.com/client/" CACHE STRING "URL for updater" )
 set( APPLICATION_ICON_NAME  "opencloud" )
-#set( APPLICATION_VIRTUALFILE_SUFFIX "owncloud" CACHE STRING "Virtual file suffix (not including the .)")
 
 set( THEME_CLASS            "OpenCloudTheme" )
 set( APPLICATION_REV_DOMAIN "eu.opencloud.desktopclient" )
@@ -13,5 +11,4 @@ set( APPLICATION_REV_DOMAIN "eu.opencloud.desktopclient" )
 set( THEME_INCLUDE          "opencloudtheme.h" )
 
 option( WITH_CRASHREPORTER "Build crashreporter" OFF )
-#set( CRASHREPORTER_SUBMIT_URL "https://crash-reports.owncloud.com/submit" CACHE STRING "URL for crash reporter" )
 
