423a08f9fb2a08d6831f52f5f3fd44568e59ab3e
Hannah von Reth <h.vonreth@opencloud.eu>
2025-02-20 17:14:34 +0100

    Add ms store icons

--- src/resources/theme/resize_svg.py
+++ src/resources/theme/resize_svg.py
@@ -5,3 +5,6 @@ themeDir = Path(__file__).parent
 for res in [16, 24, 32, 48, 64, 128, 256,  512, 1024]:
     subprocess.run(["inkscape", themeDir / "universal/opencloud-icon.svg",  "-w", str(res), "-h", str(res),  "--export-filename", themeDir / f"colored/{res}-opencloud-icon.png"])
     subprocess.run(["inkscape", themeDir / "universal/opencloud-icon-sidebar.svg",  "-w", str(res), "-h", str(res),  "--export-filename", themeDir / f"colored/{res}-opencloud-icon-sidebar.png"])
+
+for res in [44, 150, 310]:
+    subprocess.run(["inkscape", themeDir / "universal/opencloud-icon.svg",  "-w", str(res), "-h", str(res),  "--export-filename", themeDir / f"colored/{res}-opencloud-icon-ms.png"])
