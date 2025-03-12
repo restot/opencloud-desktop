638a8ab9ca89cc961678d4752af4d78c671d367e
Hannah von Reth <h.vonreth@opencloud.eu>
2025-03-12 09:53:20 +0100

    Add note about MAC_INSTALLER_BACKGROUND_FILE

--- OPENCLOUD.cmake
+++ OPENCLOUD.cmake
@@ -5,6 +5,9 @@ set( APPLICATION_DOMAIN     "opencloud.eu" )
 set( APPLICATION_VENDOR     "OpenCloud" )
 set( APPLICATION_ICON_NAME  "opencloud" )
 
+# TODO: re enable once we got icons
+#set( MAC_INSTALLER_BACKGROUND_FILE "${CMAKE_SOURCE_DIR}/admin/osx/installer-background.png")
+
 set( THEME_CLASS            "OpenCloudTheme" )
 set( APPLICATION_REV_DOMAIN "eu.opencloud.desktop" )
 
