f51913a61bb37e78f16d5b9ed8db7708ecee65cd
Hannah von Reth <h.vonreth@opencloud.eu>
2025-03-13 14:25:22 +0100

    Cache FontIcon pixmaps

--- src/resources/fonticon.cpp
+++ src/resources/fonticon.cpp
@@ -7,6 +7,7 @@
 
 #include <QIconEngine>
 #include <QPainter>
+#include <QPixmapCache>
 
 using namespace OCC::Resources;
 
@@ -25,7 +26,6 @@ public:
     void paint(QPainter *painter, const QRect &rect, QIcon::Mode, QIcon::State) override
     {
         painter->save();
-        painter->fillRect(rect, Qt::transparent);
         painter->setRenderHint(QPainter::Antialiasing);
 
         auto pen = painter->pen();
@@ -50,23 +50,45 @@ public:
             font.setPixelSize(rect.height() / 2.0);
             break;
         }
-        painter->setFont(font);
 
-        painter->drawText(rect, Qt::AlignHCenter | Qt::AlignVCenter, _glyph);
+        // inspired by https://github.com/gamecreature/QtAwesome/
+        const auto flags = Qt::AlignHCenter | Qt::AlignVCenter;
+        QFontMetricsF metrics(font);
+        QRectF boundingRect = metrics.boundingRect(rect, flags, _glyph);
+        if (boundingRect.width() > rect.width()) {
+            auto drawSize =
+                static_cast<int>(font.pixelSize() * qMin(rect.width() * 0.95 / boundingRect.width(), metrics.height() * 0.95 / boundingRect.height()));
+            font.setPixelSize(drawSize);
+        }
+
+        painter->setFont(font);
+        painter->drawText(rect, flags, _glyph);
         painter->restore();
     }
 
     QPixmap pixmap(const QSize &size, QIcon::Mode mode, QIcon::State state) override
     {
-        QPixmap pixmap(size);
-        pixmap.fill(Qt::transparent); // we need transparency
+        QPixmap pixmap;
+        const QString key = pixmapKey(size, mode, state);
+        if (QPixmapCache::find(key, &pixmap)) {
+            return pixmap;
+        }
+
+        pixmap = QPixmap(size);
+        pixmap.fill(Qt::transparent);
         QPainter painter(&pixmap);
         paint(&painter, {{0, 0}, size}, mode, state);
         painter.end();
+        QPixmapCache::insert(key, pixmap);
         return pixmap;
     }
 
     QIconEngine *clone() const override { return new FontIconEngine(this->_family, this->_glyph, this->_size); }
+
+    QString pixmapKey(const QSize &size, QIcon::Mode mode, QIcon::State state)
+    {
+        return QStringLiteral("FontIcon:%1").arg(QString::number(qHashMulti(0, _family, _glyph, _size, size, mode, state), 16));
+    }
 
     const FontIcon::FontFamily _family;
     const QChar _glyph;
