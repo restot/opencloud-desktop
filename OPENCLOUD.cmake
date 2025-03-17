8935e52c427ed9d3036c233989144d02dac299be
Hannah von Reth <h.vonreth@opencloud.eu>
2025-03-17 16:45:12 +0100

    Remove unused APPLICATION_DOMAIN

--- OPENCLOUD.cmake
+++ OPENCLOUD.cmake
@@ -1,7 +1,6 @@
 set( APPLICATION_NAME       "OpenCloud Desktop" )
 set( APPLICATION_SHORTNAME  "OpenCloud" )
 set( APPLICATION_EXECUTABLE "opencloud" )
-set( APPLICATION_DOMAIN     "opencloud.eu" )
 set( APPLICATION_VENDOR     "OpenCloud" )
 set( APPLICATION_ICON_NAME  "opencloud" )
 
