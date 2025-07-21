5c649ee0ec3a387494255ea1a611f171011e124b
Hannah von Reth <h.vonreth@opencloud.eu>
2025-07-21 16:00:38 +0200

    Fix duration

--- src/libsync/common/chronoelapsedtimer.cpp
+++ src/libsync/common/chronoelapsedtimer.cpp
@@ -45,7 +45,7 @@ void ChronoElapsedTimer::stop()
     Q_ASSERT(_end == steady_clock::time_point{});
     Q_ASSERT(_started);
     _end = steady_clock::now();
-    _started = false;
+    // don't set _started to false, so that we can still call duration() later
 }
 
 nanoseconds ChronoElapsedTimer::duration() const
