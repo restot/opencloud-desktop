opencloud-eu/desktop

test: add test for replacing a pdf file

test/gui/shared/steps/file_context.py
@@ -373,3 +373,25 @@ def step(context, folder_name):
     remember_path(folder_path)
     # when account is added, folder with suffix will be created
     remember_path(f'{folder_path} (2)')
+
+
+@Given(
+    'user "|any|" has created a file "|any|" with the following content inside the sync folder'
+)
+def step(context, username, filename):
+    file_content = '\n'.join(context.multiLineText)
+    file = get_resource_path(filename, username)
+    wait_and_write_file(convert_path_separators_for_os(file), file_content)
+
+
+@When(
+    r'user "([^"]*)" replaces file "([^"]*)" with "([^"]*)" in the sync folder',
+    regexp=True,
+)
+def step(context, username, destination, source):
+    wait_for_client_to_be_ready()
+    source_dir = get_resource_path(source, username)
+    if destination in (None, '/'):
+        destination = ''
+    destination_dir = get_resource_path(destination, username)
+    shutil.copy(source_dir, destination_dir)

test/gui/tst_editFiles/test.feature
@@ -25,3 +25,23 @@ Feature: edit files
         And the user overwrites the file "testfile.txt" with content "overwrite openCloud test text file"
         And the user waits for file "testfile.txt" to be synced
         Then as "Alice" the file "testfile.txt" should have the content "overwrite openCloud test text file" in the server
+
+
+   Scenario: Replace and modify the content of a pdf file multiple times
+     Given user "Alice" has set up a client with default settings
+     And user "Alice" has created a file "file1.pdf" with the following content inside the sync folder
+            """
+            to be replaced
+            """
+     And user "Alice" has created a folder "Folder1" inside the sync folder
+     And user "Alice" has created a file "Folder1/file1.pdf" with the following content inside the sync folder
+            """
+            to replace file inside root sync folder
+            """
+     When user "Alice" replaces file "file1.pdf" with "Folder1/file1.pdf" in the sync folder
+     And the user waits for the files to sync
+     And the user overwrites the file "file1.pdf" with content "overwritten to replace file inside root sync folder"
+     And the user waits for file "file1.pdf" to be synced
+     And the user overwrites the file "file1.pdf" with content "overwritten again to replace file inside root sync folder"
+     And the user waits for file "file1.pdf" to be synced
+     Then as "Alice" the file "file1.pdf" should have the content "overwritten again to replace file inside root sync folder" in the server

restot/opencloud-desktop

test/gui/shared/steps/file_context.py
@@ -100,6 +100,11 @@ def add_copy_suffix(resource_path, resource_type):
     return resource_path + ' - Copy'
 
 
+def overwrite_file(resource, content):
+    resource = get_resource_path(resource)
+    wait_and_write_file(resource, content)
+
+
 @When(
     'user "|any|" creates a file "|any|" with the following content inside the sync folder'
 )
@@ -223,8 +228,7 @@ def step(context, filename):
 
 @When('the user overwrites the file "|any|" with content "|any|"')
 def step(context, resource, content):
-    resource = get_resource_path(resource)
-    wait_and_write_file(resource, content)
+    overwrite_file(resource, content)
 
 
 @When('the user tries to overwrite the file "|any|" with content "|any|"')
@@ -373,3 +377,21 @@ def step(context, folder_name):
     remember_path(folder_path)
     # when account is added, folder with suffix will be created
     remember_path(f'{folder_path} (2)')
+
+
+@When(
+    r'user "([^"]*)" replaces file "([^"]*)" with "([^"]*)" in the sync folder',
+    regexp=True,
+)
+def step(context, username, destination, source):
+    wait_for_client_to_be_ready()
+    source_dir = get_resource_path(source, username)
+    if destination in (None, '/'):
+        destination = ''
+    destination_dir = get_resource_path(destination, username)
+    shutil.copy(source_dir, destination_dir)
+    
+    
+@Given('the user has overwritten the file "|any|" with content "|any|"')
+def step(context, resource, content):
+    overwrite_file(resource, content)

test/gui/tst_editFiles/test.feature
@@ -25,3 +25,18 @@ Feature: edit files
         And the user overwrites the file "testfile.txt" with content "overwrite openCloud test text file"
         And the user waits for file "testfile.txt" to be synced
         Then as "Alice" the file "testfile.txt" should have the content "overwrite openCloud test text file" in the server
+
+
+   Scenario: Replace and modify the content of a pdf file multiple times
+     Given user "Alice" has uploaded file "simple.pdf" to "/simple.pdf" in the server
+     And user "Alice" has created folder "simple-folder" in the server
+     And user "Alice" has uploaded file "simple.pdf" to "simple-folder/simple.pdf" in the server
+     And user "Alice" has set up a client with default settings
+     And the user has overwritten the file "simple-folder/simple.pdf" with content "overwrite openCloud test pdf file"
+     When user "Alice" replaces file "simple.pdf" with "simple-folder/simple.pdf" in the sync folder
+     And the user waits for the files to sync
+     And the user overwrites the file "simple.pdf" with content "overwritten to replace file inside root sync folder"
+     And the user waits for file "simple.pdf" to be synced
+     And the user overwrites the file "simple.pdf" with content "overwritten again to replace file inside root sync folder"
+     And the user waits for file "simple.pdf" to be synced
+     Then as "Alice" the file "simple.pdf" should have the content "overwritten again to replace file inside root sync folder" in the server