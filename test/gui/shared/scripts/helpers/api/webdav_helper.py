opencloud-eu/desktop

test: add stting up client with Shares space

test/gui/shared/scripts/helpers/api/webdav_helper.py
@@ -4,18 +4,10 @@ import json
 
 import helpers.api.http_helper as request
 from helpers.api.utils import url_join
-from helpers.ConfigHelper import get_config
+from helpers.ConfigHelper import get_config, PERMISSION_ROLES
 from helpers.FilesHelper import get_file_for_upload
-from helpers.SpaceHelper import get_space_id, get_personal_space_id
+from helpers.SpaceHelper import get_personal_space_id
 from helpers.api import provisioning
-
-
-# Permission roles mapping
-PERMISSION_ROLES = {
-    'Secure Viewer': 'aa97fe03-7980-45ac-9e50-b325749fd7e6',
-    'Space Editor Without Versions': '3284f2d5-0070-4ad8-ac40-c247f7c1fb27',
-    'Denied': '63e64e19-8d43-42ec-a738-2b6af2610efa',
-}
 
 
 def get_webdav_url():
@@ -52,10 +44,8 @@ def get_file_content(user, resource):
 
 def get_folder_items_count(user, folder_name):
     folder_name = folder_name.strip('/')
-    path = get_resource_path(user, folder_name)
-    xml_response = request.propfind(path, user=user)
+    root_element = get_propfind_root_element(user, folder_name)
     total_items = 0
-    root_element = ET.fromstring(xml_response.content)
     for response_element in root_element:
         for href_element in response_element:
             # The first item is folder itself so excluding it
@@ -96,15 +86,19 @@ def delete_resource(user, resource):
     assert response.status_code == 204, f"Could not delete folder '{resource}'"
 
 
-def get_resource_id(user, resource):
-    """Get the resource ID for a given resource path"""
+def get_propfind_root_element(user, resource):
+    """Get the root XML element from a PROPFIND request"""
     path = get_resource_path(user, resource)
     xml_response = request.propfind(path, user=user)
 
     if xml_response.status_code != 207:
         raise AssertionError(f'Failed to get resource properties: {xml_response.status_code}')
 
-    root_element = ET.fromstring(xml_response.content)
+    return ET.fromstring(xml_response.content)
+
+
+def get_resource_id(user, resource):
+    root_element = get_propfind_root_element(user, resource)
 
     # Find the first response element (the resource itself)
     for response_element in root_element:
@@ -116,7 +110,6 @@ def get_resource_id(user, resource):
                             for child in prop:
                                 if child.tag == '{http://owncloud.org/ns}fileid':
                                     return child.text
-            break  # Only process the first response (the resource itself)
 
     raise AssertionError(f'Could not find resource ID for {resource}')
 

test/gui/tst_syncing/test.feature
@@ -496,11 +496,29 @@ Feature: Syncing files
         And as "Alice" folder "simple-folder" should exist in the server
 
 
-    Scenario: Sync a received shared folder
-        Given user "Alice" has created folder "simple-folder" in the server
-        And user "Brian" has been created in the server with default attributes
+    Scenario: Sync a received shared folder with Viewer permission role
+        Given user "Brian" has been created in the server with default attributes
+        And user "Alice" has created folder "simple-folder" in the server
         And user "Alice" has uploaded file with content "test content" to "simple-folder/uploaded-lorem.txt" in the server
         And user "Alice" has sent the following resource share invitation:
             | resource        | simple-folder |
             | sharee          | Brian         |
-            | permissionsRole | Secure Viewer |
+            | permissionsRole | Viewer        |
+        And user "Brian" has set up a client with space "Shares"
+        When user "Brian" creates a folder "simple-folder/sub-folder" inside the sync folder
+        And the user copies file "simple.pdf" from outside the sync folder to "simple-folder/simple.pdf" in the sync folder
+        Then the folder "simple-folder/sub-folder" should exist on the file system
+        And the file "simple-folder/simple.pdf" should exist on the file system
+        But the following error message should appear in the client
+            """
+            simple-folder/sub-folder: Not allowed because you don't have permission to add subfolders to that folder
+            """
+        And as "Brian" folder "simple-folder/sub-folder" should not exist in the server
+        And as "Brian" file "simple-folder/simple.pdf" should not exist in the server
+        When the user clicks on the activity tab
+        And the user selects "Not Synced" tab in the activity
+        Then the following activities should be displayed in not synced table
+            | resource                 | status      | account                              |
+            | simple-folder/sub-folder | Blacklisted | Brian Murphy@%local_server_hostname% |
+            | simple-folder/simple.pdf | Blacklisted | Brian Murphy@%local_server_hostname% |
+

restot/opencloud-desktop

test/gui/shared/scripts/helpers/api/webdav_helper.py
@@ -4,18 +4,10 @@ import json
 
 import helpers.api.http_helper as request
 from helpers.api.utils import url_join
-from helpers.ConfigHelper import get_config
+from helpers.ConfigHelper import get_config, PERMISSION_ROLES
 from helpers.FilesHelper import get_file_for_upload
-from helpers.SpaceHelper import get_space_id, get_personal_space_id
+from helpers.SpaceHelper import get_personal_space_id
 from helpers.api import provisioning
-
-
-# Permission roles mapping
-PERMISSION_ROLES = {
-    'Secure Viewer': 'aa97fe03-7980-45ac-9e50-b325749fd7e6',
-    'Space Editor Without Versions': '3284f2d5-0070-4ad8-ac40-c247f7c1fb27',
-    'Denied': '63e64e19-8d43-42ec-a738-2b6af2610efa',
-}
 
 
 def get_webdav_url():
@@ -50,12 +42,21 @@ def get_file_content(user, resource):
     return response.content
 
 
+def get_folder_items(user, resource):
+    """Get the root XML element from a PROPFIND request"""
+    path = get_resource_path(user, resource)
+    xml_response = request.propfind(path, user=user)
+
+    if xml_response.status_code != 207:
+        raise AssertionError(f'Failed to get resource properties: {xml_response.status_code}')
+
+    return ET.fromstring(xml_response.content)
+
+
 def get_folder_items_count(user, folder_name):
     folder_name = folder_name.strip('/')
-    path = get_resource_path(user, folder_name)
-    xml_response = request.propfind(path, user=user)
+    root_element = get_folder_items(user, folder_name)
     total_items = 0
-    root_element = ET.fromstring(xml_response.content)
     for response_element in root_element:
         for href_element in response_element:
             # The first item is folder itself so excluding it
@@ -97,26 +98,14 @@ def delete_resource(user, resource):
 
 
 def get_resource_id(user, resource):
-    """Get the resource ID for a given resource path"""
-    path = get_resource_path(user, resource)
-    xml_response = request.propfind(path, user=user)
+    root_element = get_folder_items(user, resource)
 
-    if xml_response.status_code != 207:
-        raise AssertionError(f'Failed to get resource properties: {xml_response.status_code}')
+    # Finding the fileid elements using XPath with namespace notation
+    fileid_elements = root_element.findall(".//{http://owncloud.org/ns}fileid")
 
-    root_element = ET.fromstring(xml_response.content)
-
-    # Find the first response element (the resource itself)
-    for response_element in root_element:
-        if response_element.tag == '{DAV:}response':
-            for propstat in response_element:
-                if propstat.tag == '{DAV:}propstat':
-                    for prop in propstat:
-                        if prop.tag == '{DAV:}prop':
-                            for child in prop:
-                                if child.tag == '{http://owncloud.org/ns}fileid':
-                                    return child.text
-            break  # Only process the first response (the resource itself)
+    # The First element is the desired resource's file id
+    if fileid_elements:
+        return fileid_elements[0].text
 
     raise AssertionError(f'Could not find resource ID for {resource}')
 

test/gui/tst_syncing/test.feature
@@ -496,11 +496,32 @@ Feature: Syncing files
         And as "Alice" folder "simple-folder" should exist in the server
 
 
-    Scenario: Sync a received shared folder
-        Given user "Alice" has created folder "simple-folder" in the server
-        And user "Brian" has been created in the server with default attributes
+    Scenario: Sync a received shared folder with Viewer permission role
+        Given user "Brian" has been created in the server with default attributes
+        And user "Alice" has created folder "simple-folder" in the server
         And user "Alice" has uploaded file with content "test content" to "simple-folder/uploaded-lorem.txt" in the server
         And user "Alice" has sent the following resource share invitation:
             | resource        | simple-folder |
             | sharee          | Brian         |
-            | permissionsRole | Secure Viewer |
+            | permissionsRole | Viewer        |
+        And user "Brian" has set up a client with space "Shares"
+        When user "Brian" creates a folder "simple-folder/sub-folder" inside the sync folder
+        Then the folder "simple-folder/sub-folder" should exist on the file system
+        But the following error message should appear in the client
+            """
+            simple-folder/sub-folder: Not allowed because you don't have permission to add subfolders to that folder
+            """
+        When the user copies file "simple.pdf" from outside the sync folder to "simple-folder/simple.pdf" in the sync folder
+        Then the file "simple-folder/simple.pdf" should exist on the file system
+        But the following error message should appear in the client
+            """
+            simple-folder/simple.pdf: Not allowed because you don't have permission to add files in that folder
+            """
+        And as "Brian" folder "simple-folder/sub-folder" should not exist in the server
+        And as "Brian" file "simple-folder/simple.pdf" should not exist in the server
+        When the user clicks on the activity tab
+        And the user selects "Not Synced" tab in the activity
+        Then the following activities should be displayed in not synced table
+            | resource                 | status      | account                              |
+            | simple-folder/sub-folder | Blacklisted | Brian Murphy@%local_server_hostname% |
+            | simple-folder/simple.pdf | Blacklisted | Brian Murphy@%local_server_hostname% |