aca126408eb1688301cf8f0be1f5d0c53a822f94
Hannah von Reth <h.vonreth@opencloud.eu>
2025-10-15 10:15:01 +0200

    Fix cmake warning, MAIN_DEPENDENCY is redundant and irgnored on add_custom_command(TARGET

--- cmake/modules/OCAddVfsPlugin.cmake
+++ cmake/modules/OCAddVfsPlugin.cmake
@@ -22,7 +22,7 @@ function(add_vfs_plugin)
         set_target_properties(vfs_${__PLUGIN_NAME} PROPERTIES LIBRARY_OUTPUT_DIRECTORY "$<TARGET_FILE_DIR:opencloud>/../PlugIns/")
         # make the plugins available to the tests
         add_custom_command(TARGET vfs_${__PLUGIN_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND}
-            ARGS -E create_symlink "$<TARGET_FILE:vfs_${__PLUGIN_NAME}>" "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/$<TARGET_FILE_NAME:vfs_${__PLUGIN_NAME}>" MAIN_DEPENDENCY "vfs_${__PLUGIN_NAME}")
+            ARGS -E create_symlink "$<TARGET_FILE:vfs_${__PLUGIN_NAME}>" "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/$<TARGET_FILE_NAME:vfs_${__PLUGIN_NAME}>")
 
     else()
         install(TARGETS vfs_${__PLUGIN_NAME} DESTINATION "${KDE_INSTALL_PLUGINDIR}")
