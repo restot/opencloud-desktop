e7f2d6ca794645876329ee8b8a1be43e4198c277
Hannah von Reth <h.vonreth@opencloud.eu>
2025-10-16 10:45:41 +0200

    Modify APPLICATION_REV_DOMAIN in beta builds to make coinstallable

--- OPENCLOUD.cmake
+++ OPENCLOUD.cmake
@@ -3,18 +3,19 @@ set( APPLICATION_SHORTNAME  "OpenCloud" )
 set( APPLICATION_EXECUTABLE "opencloud" )
 set( APPLICATION_VENDOR     "OpenCloud" )
 set( APPLICATION_ICON_NAME  "opencloud" )
+set( APPLICATION_REV_DOMAIN "eu.opencloud.desktop" )
 
 if(BETA_CHANNEL_BUILD)
     set( APPLICATION_NAME       "${APPLICATION_NAME} Beta")
     set( APPLICATION_SHORTNAME  "${APPLICATION_SHORTNAME} Beta" )
     set( APPLICATION_EXECUTABLE "${APPLICATION_EXECUTABLE}_beta" )
     set( APPLICATION_VENDOR     "${APPLICATION_VENDOR} Beta" )
+    set( APPLICATION_REV_DOMAIN "${APPLICATION_REV_DOMAIN}_beta" )
 endif()
 # TODO: re enable once we got icons
 #set( MAC_INSTALLER_BACKGROUND_FILE "${CMAKE_SOURCE_DIR}/admin/osx/installer-background.png")
 
 set( THEME_CLASS            "OpenCloudTheme" )
-set( APPLICATION_REV_DOMAIN "eu.opencloud.desktop" )
 
 set( THEME_INCLUDE          "opencloudtheme.h" )
 
